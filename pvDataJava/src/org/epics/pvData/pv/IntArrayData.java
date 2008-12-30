92982400ab4c56a03046fb8adde3754cd34a4699
mrkraimer <unknown>
2008-12-30 13:58:32 +0000

    Initial CVS Commit

---
+++ pvDataJava/src/org/epics/pvData/pv/IntArrayData.java
@@ -0,0 +1,26 @@
+/**
+ * Copyright - See the COPYRIGHT that is included with this distribution.
+ * EPICS JavaIOC is distributed subject to a Software License Agreement found
+ * in file LICENSE that is included with this distribution.
+ */
+package org.epics.pvData.pv;
+/**
+ * Class required by get/put PVIntArray methods.
+ * Get will set data and offset.
+ * @author mrk
+ *
+ */
+public class IntArrayData {
+    /**
+     * The int[].
+     * PVIntArray.get sets this value.
+     * PVIntArray.put requires that the caller set the value. 
+     */
+    public int[] data;
+    /**
+     * The offset.
+     * PVIntArray.get sets this value.
+     * PVIntArray.put requires that the caller set the value. 
+     */
+    public int offset;
+}
