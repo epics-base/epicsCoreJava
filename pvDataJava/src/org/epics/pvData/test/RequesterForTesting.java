d7c5c0945be54dc10ee5ae51367058d8a2d2e2bb
mrkraimer <unknown>
2008-12-30 13:58:34 +0000

    Initial CVS Commit

---
+++ pvDataJava/src/org/epics/pvData/test/RequesterForTesting.java
@@ -0,0 +1,32 @@
+/**
+ * 
+ */
+package org.epics.pvData.test;
+import org.epics.pvData.pv.MessageType;
+import org.epics.pvData.pv.Requester;
+
+/**
+ * @author mrk
+ *
+ */
+public class RequesterForTesting implements Requester {
+	private String requesterName = null;
+	
+	RequesterForTesting(String requesterName) {
+		this.requesterName = requesterName;
+	}
+	/* (non-Javadoc)
+     * @see org.epics.ioc.util.Requester#getRequestorName()
+     */
+    public String getRequesterName() {
+        return requesterName;
+    }
+
+    /* (non-Javadoc)
+     * @see org.epics.ioc.util.Requester#message(java.lang.String, org.epics.ioc.util.MessageType)
+     */
+    public void message(String message, MessageType messageType) {
+        System.out.println(message);
+        
+    }
+}
