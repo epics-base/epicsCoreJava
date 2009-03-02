6f11ab4b0884c2c2a661a6e858e9b6b72c144951
mrkraimer <unknown>
2009-03-02 19:11:37 +0000

    add stop method to Executor
    Add TimeFunction support
    
    Add test to compute threadSwitch time.

--- pvDataJava/src/org/epics/pvData/misc/RunnableReady.java
+++ pvDataJava/src/org/epics/pvData/misc/RunnableReady.java
@@ -1,6 +1,6 @@
 /**
  * Copyright - See the COPYRIGHT that is included with this distribution.
- * EPICS JavaIOC is distributed subject to a Software License Agreement found
+ * EPICS pvData is distributed subject to a Software License Agreement found
  * in file LICENSE that is included with this distribution.
  */
 package org.epics.pvData.misc;
@@ -13,7 +13,7 @@ package org.epics.pvData.misc;
 public interface RunnableReady {
      /**
       * The run method.
-     * @param threadReady threadReady.ready() must be called when run is reasdy.
+     * @param threadReady threadReady.ready() must be called when run is ready.
      */
     void run(ThreadReady threadReady);
 }
