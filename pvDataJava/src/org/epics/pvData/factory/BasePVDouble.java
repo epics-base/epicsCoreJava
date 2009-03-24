c3c8e9befba4a54596dbd1a9e4d93ef1f6289dd4
msekoranja <unknown>
2009-03-24 16:38:28 +0000

    removed serialization size and serialization of interfaces (too specific)

--- pvDataJava/src/org/epics/pvData/factory/BasePVDouble.java
+++ pvDataJava/src/org/epics/pvData/factory/BasePVDouble.java
@@ -54,12 +54,6 @@ public class BasePVDouble extends AbstractPVScalar implements PVDouble
         + super.toString(indentLevel);
     }
     /* (non-Javadoc)
-     * @see org.epics.pvData.pv.Serializable#getSerializationSize()
-     */
-    public int getSerializationSize() {
-        return 8;
-    }
-    /* (non-Javadoc)
      * @see org.epics.pvData.pv.Serializable#serialize(java.nio.ByteBuffer)
      */
     public void serialize(ByteBuffer buffer) {
