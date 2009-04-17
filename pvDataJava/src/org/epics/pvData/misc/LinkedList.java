a1c5d90b99bc3a73727179f0c8be0751d7b57e86
mrkraimer <unknown>
2009-04-17 18:58:51 +0000

    LinkedList is new. Executor interface changed so that implementation can use LinkedList

--- pvDataJava/src/org/epics/pvData/misc/LinkedList.java
+++ pvDataJava/src/org/epics/pvData/misc/LinkedList.java
@@ -7,88 +7,95 @@ package org.epics.pvData.misc;
 
 /**
  * Linked List with user supplied storage for node.
- * The user must create nodes by calling LinkedListFactory.createNode.
+ * The user must create nodes by calling LinkedListCreate.createNode.
  * A node can be put on any LinkedList but only on one list at a time.
+ * It is the users responsibility to synchronize access to the list.
  * @author mrk
  *
  */
-public interface LinkedList {
+public interface LinkedList<T> {
+    int getLength();
     /**
      * Add a node to the end of the list.
      * @param listNode The node to add to the list.
      * @throws IllegalStateException if the node is already on the list.
      */
-    void addTail(ListNode listNode);
+    void addTail(LinkedListNode<T> listNode);
     /**
      * Add a node to the beginning of the list.
      * @param listNode The node to add to the list.
      * @throws IllegalStateException if the node is already on the list.
      */
-    void addHead(ListNode listNode);
+    void addHead(LinkedListNode<T> listNode);
     /**
      * Add a node after a node that is already on the list.
      * @param listNode The node that is on the list.
      * @param addNode The node to add.
      * @throws IllegalStatexception if listNode is not on list or addNode is already on list.
      */
-    void insertAfter(ListNode listNode,ListNode addNode);
+    void insertAfter(LinkedListNode<T> listNode,LinkedListNode<T> addNode);
     /**
      * Add a node before a node that is already on the list.
      * @param listNode The node that is on the list.
      * @param addNode The node to add.
      * @throws IllegalStatexception if listNode is not on list or addNode is already on list.
      */
-    void insertBefore(ListNode listNode,ListNode addNode);   
+    void insertBefore(LinkedListNode<T> listNode,LinkedListNode<T> addNode);   
     /**
-     * Remove and return the ListNode that is at the end of the list.
+     * Remove and return the LinkedListNode that is at the end of the list.
      * @return The listNode or null if the list is empty.
      */
-    ListNode removeTail();
+    LinkedListNode<T> removeTail();
     /**
-     * Remove and return the ListNode that is at the head of the list.
+     * Remove and return the LinkedListNode that is at the head of the list.
      * @return The listNode or null if the list is empty.
      */
-    ListNode removeHead();
+    LinkedListNode<T> removeHead();
     /**
      * Remove the listNode from the list.
      * @param listNode The node to remove.
      */
-    void remove(ListNode listNode);
+    void remove(LinkedListNode<T> listNode);
+    /**
+     * Remove the object from the list.
+     * @param object The object to remove.
+     */
+    void remove(T object);
     /**
      * Get the node at the head of the list.
      * The node is not removed from the list.
      * @return The node or null if the list is empty.
      */
-    ListNode getHead();
+    LinkedListNode<T> getHead();
     /**
      * Get the node at the end of the list.
      * The node is not removed from the list.
      * @return The node or null if the list is empty.
      */
-    ListNode getTail();
+    LinkedListNode<T> getTail();
     /**
      * Get the node after listNode.
      * @param listNode The current node.
      * @return The node or null if listNode is the last node on the list.
      * @throws IllegalStateException if listNode is not on the list.
      */
-    ListNode getNext(ListNode listNode);
+    LinkedListNode<T> getNext(LinkedListNode<T> listNode);
     /**
      * Get the node before listNode.
      * @param listNode The current node.
      * @return The node or null if listNode is the first node on the list.
      * @throws IllegalStateException if listNode is not on the list.
      */
-    ListNode getPrev(ListNode listNode);
+    LinkedListNode<T> getPrev(LinkedListNode<T> listNode);
     /**
      * Is the list empty?
      * @return (false,true) if the list (is not,is) empty.
      */
     boolean isEmpty();
     /**
-     * Is the node on a list?
-     * @param listNode The node.
-     * @return (false,true) if the node (is not,is) on a list.
+     * Is the object on the list?
+     * @param object The object.
+     * @return (false,true) if the node (is not, is on the list).
      */
-    boolean isOnList(ListNode listNode);
+    boolean contains(T object);
 }
---
+++ pvDataJava/src/org/epics/pvData/misc/LinkedListArray.java
@@ -0,0 +1,39 @@
+/**
+ * Copyright - See the COPYRIGHT that is included with this distribution.
+ * EPICS pvData is distributed subject to a Software License Agreement found
+ * in file LICENSE that is included with this distribution.
+ */
+package org.epics.pvData.misc;
+
+/**
+ * Convert a LinkedList to a LinkedListNode array.
+ * LinkedListCreate provides an efficient implementation.
+ * @author mrk
+ *
+ */
+public interface LinkedListArray<T> {
+    /**
+     * Set the LinkNode array from linkedList.
+     * If the current capacity of the array is less than the number of elements in the current array a new array
+     * is allocated. The values returned by getNodes and getLength are determined by the argument passed to setNodes.
+     * The linkedList is traversed by getHead and getNext.
+     * @param linkedList The list.
+     */
+    void setNodes(LinkedList<T> linkedList);
+    /**
+     * The LinkedListNode array as set by the last call to setNodes.
+     * @return The LinkedListNode array. The first getLength() elements are not null and the remaining elements are all null.
+     */
+    LinkedListNode<T>[] getNodes();
+    /**
+     * The number of non-null elements in the array returned by getNodes.
+     * @return the length.
+     */
+    int getLength();
+    /**
+     * Clear the array.
+     * The array is set to null.
+     * 
+     */
+    void clear();
+}
