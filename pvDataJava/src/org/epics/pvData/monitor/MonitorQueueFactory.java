989befe66a84a243cf861573d4ef87eef9a4edb3
mrkraimer <unknown>
2009-08-19 19:04:00 +0000

    use pvData.misc.Queue for implementation of MonitorQueue

--- pvDataJava/src/org/epics/pvData/monitor/MonitorQueueFactory.java
+++ pvDataJava/src/org/epics/pvData/monitor/MonitorQueueFactory.java
@@ -5,7 +5,7 @@
  */
 package org.epics.pvData.monitor;
 
-import org.epics.pvData.misc.BitSet;
+import org.epics.pvData.misc.*;
 import org.epics.pvData.pv.PVStructure;
 import org.epics.pvData.pvCopy.PVCopy;
 
@@ -15,6 +15,7 @@ import org.epics.pvData.pvCopy.PVCopy;
  *
  */
 public class MonitorQueueFactory {
+    
     /**
      * Create a MonitorQueue.
      * @param pvCopy The PVCopy to create the data in each queue element.
@@ -26,18 +27,22 @@ public class MonitorQueueFactory {
         if(queueSize<2) {
             throw new IllegalStateException("queueSize must be at least 2");
         }
-        MonitorQueue.MonitorQueueElement[] monitorQueueElements = new MonitorQueue.MonitorQueueElement[queueSize];
-        for(int i=0; i<monitorQueueElements.length; i++) {
+        QueueElement<MonitorElement>[] queueElements = new QueueElement[queueSize];
+        for(int i=0; i<queueElements.length; i++) {
             PVStructure pvStructure = pvCopy.createPVStructure();
-            monitorQueueElements[i] = new MonitorQueueElementImlp(pvStructure);
-            
+            MonitorElementImlp monitorElement = new MonitorElementImlp(pvStructure);
+            QueueElement<MonitorElement> queueElement = queueCreate.createQueueElement(monitorElement);
+            monitorElement.setQueueElement(queueElement);
+            queueElements[i] = queueElement;
         }
-        return new MonitorQueueImpl(monitorQueueElements);
+        Queue<MonitorElement> queue = queueCreate.create(queueElements);
+        return new MonitorQueueImpl(queue);
     }
     
+    private static QueueCreate<MonitorElement> queueCreate = new QueueCreate<MonitorElement>();
     
-    private static class MonitorQueueElementImlp implements MonitorQueue.MonitorQueueElement {
-        MonitorQueueElementImlp(PVStructure pvStructure) {
+    private static class MonitorElementImlp implements MonitorElement {
+        MonitorElementImlp(PVStructure pvStructure) {
             this.pvStructure = pvStructure;
             changedBitSet = new BitSet(pvStructure.getNumberFields());
             overrunBitSet = new BitSet(pvStructure.getNumberFields());
@@ -46,6 +51,7 @@ public class MonitorQueueFactory {
         private final PVStructure pvStructure;
         private final BitSet changedBitSet;
         private final BitSet overrunBitSet;
+        private QueueElement<MonitorElement> queueElement = null;
         
         /* (non-Javadoc)
          * @see org.epics.ioc.channelAccess.MonitorQueue.MonitorQueueElement#getChangedBitSet()
@@ -68,22 +74,21 @@ public class MonitorQueueFactory {
         public PVStructure getPVStructure() {
             return pvStructure;
         }
+        
+        private void setQueueElement(QueueElement<MonitorElement> queueElement) {
+            this.queueElement = queueElement;
+        }
+        
+        private QueueElement<MonitorElement> getQueueElement() {
+            return queueElement;
+        }
     }
+    
     private static class MonitorQueueImpl implements MonitorQueue {
-        private final MonitorQueueElement[] monitorQueueElements;
-        private final int number;
-        private int numberFree = 0;
-        private int numberUsed = 0;
-        private int nextGetFree = 0;
-        private int nextSetUsed = 0;
-        private int nextGetUsed = 0;
-        private int nextReleaseUsed = 0;
+        private final Queue<MonitorElement> queue;
 
-
-        MonitorQueueImpl(MonitorQueueElement[] monitorQueueElements) {
-            this.monitorQueueElements = monitorQueueElements;
-            number = monitorQueueElements.length;
-            numberFree = number;
+        MonitorQueueImpl(Queue<MonitorElement> queue) {
+           this.queue = queue;
         }
        
         /* (non-Javadoc)
@@ -91,79 +96,62 @@ public class MonitorQueueFactory {
          */
         @Override
         public void clear() {
-            numberFree = number;
-            numberUsed = 0;
-            nextGetFree = 0;
-            nextSetUsed = 0;
-            nextGetUsed = 0;
-            nextReleaseUsed = 0;
+           queue.clear();
         }
         /* (non-Javadoc)
          * @see org.epics.ioc.channelAccess.MonitorQueue#capacity()
          */
         @Override
         public int capacity() {
-            return number;
+            return queue.capacity();
         }
         /* (non-Javadoc)
          * @see org.epics.ioc.channelAccess.MonitorQueue#getNumberFree()
          */
         @Override
         public int getNumberFree() {
-            return numberFree;
+            return queue.getNumberFree();
         }
         /* (non-Javadoc)
-         * @see org.epics.ioc.channelAccess.MonitorQueue#getFree()
+         * @see org.epics.pvData.monitor.MonitorQueue#getFree()
          */
         @Override
-        public MonitorQueueElement getFree() {
-            if(numberFree==0) return null;
-            numberFree--;
-            MonitorQueueElement monitorQueueElement = monitorQueueElements[nextGetFree++];
-            if(nextGetFree>=number) nextGetFree = 0;
-            return monitorQueueElement;
+        public MonitorElement getFree() {
+            QueueElement<MonitorElement> queueElement = queue.getFree();
+            if(queueElement==null) return null;
+            return queueElement.getObject();
         }
         /* (non-Javadoc)
-         * @see org.epics.ioc.channelAccess.MonitorQueue#setUsed(org.epics.ioc.channelAccess.MonitorQueue.MonitorQueueElement)
+         * @see org.epics.pvData.monitor.MonitorQueue#setUsed(org.epics.pvData.monitor.MonitorElement)
          */
         @Override
-        public void setUsed(MonitorQueueElement monitorQueueElement) {
-            if(monitorQueueElement!=monitorQueueElements[nextSetUsed++]) {
-                throw new IllegalStateException("not correct monitorQueueElement");
-            }
-            numberUsed++;
-            if(nextSetUsed>=number) nextSetUsed = 0;
+        public void setUsed(MonitorElement monitorElement) {
+            MonitorElementImlp temp = (MonitorElementImlp)monitorElement;
+            queue.setUsed(temp.getQueueElement());
         }
         /* (non-Javadoc)
-         * @see org.epics.ioc.channelAccess.MonitorQueue#getUsed()
+         * @see org.epics.pvData.monitor.MonitorQueue#getUsed()
          */
         @Override
-        public MonitorQueueElement getUsed() {
-            if(numberUsed==0) return null;
-            MonitorQueueElement monitorQueueElement = monitorQueueElements[nextGetUsed++];
-            if(nextGetUsed>=number) nextGetUsed = 0;
-            return monitorQueueElement;
+        public MonitorElement getUsed() {
+            QueueElement<MonitorElement> queueElement = queue.getUsed();
+            if(queueElement==null) return null;
+            return queueElement.getObject();
         }
         /* (non-Javadoc)
-         * @see org.epics.ioc.channelAccess.MonitorQueue#releaseUsed(org.epics.ioc.channelAccess.MonitorQueue.MonitorQueueElement)
+         * @see org.epics.pvData.monitor.MonitorQueue#releaseUsed(org.epics.pvData.monitor.MonitorElement)
          */
         @Override
-        public void releaseUsed(MonitorQueueElement monitorQueueElement) {
-            if(monitorQueueElement!=monitorQueueElements[nextReleaseUsed++]) {
-                throw new IllegalStateException("not monitorQueueElement returned by last call to getUsed");
-            }
-            if(nextReleaseUsed>=number) nextReleaseUsed = 0;
-            numberUsed--;
-            numberFree++;
+        public void releaseUsed(MonitorElement monitorElement) {
+            MonitorElementImlp temp = (MonitorElementImlp)monitorElement;
+            queue.releaseUsed(temp.getQueueElement());
         }
         /* (non-Javadoc)
          * @see java.lang.Object#toString()
          */
         @Override
         public String toString() {
-            return "number " + number + " numberFree " + numberFree + " numberUsed " + numberUsed
-                 + " nextGetFree " + nextGetFree + " nextSetUsed " + nextSetUsed
-                 + " nextGetUsed " + nextGetUsed + " nextReleaseUsed " + nextReleaseUsed;
+            return queue.toString();
         }
     }
 }
