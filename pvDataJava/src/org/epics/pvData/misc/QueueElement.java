6fce1b523c58bf563439213d2741586b4be4d75e
mrkraimer <unknown>
2009-08-19 19:01:58 +0000

    Queue is new

---
+++ pvDataJava/src/org/epics/pvData/misc/QueueElement.java
@@ -0,0 +1,20 @@
+/**
+ * Copyright - See the COPYRIGHT that is included with this distribution.
+ * EPICS JavaIOC is distributed subject to a Software License Agreement found
+ * in file LICENSE that is included with this distribution.
+ */
+package org.epics.pvData.misc;
+
+/**
+ * Interface for a QueueElement.
+ * Code that uses Queue must supply a node by calling QueueCreate.crateElement().
+ * @author mrk
+ *
+ */
+public interface QueueElement<T> {
+    /**
+     * Get the object passed to QueueCreate.createElement();
+     * @return the object.
+     */
+    public T getObject();
+}
