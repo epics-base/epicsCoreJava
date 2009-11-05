1e8cee8149045e36f04d8490a8edfb4fb3e8e749
msekoranja <unknown>
2009-11-05 17:19:29 +0000

    Partital serialization/deserialization.

--- pvDataJava/src/org/epics/pvData/pvCopy/BitSetSerializable.java
+++ pvDataJava/src/org/epics/pvData/pvCopy/BitSetSerializable.java
@@ -8,6 +8,8 @@ package org.epics.pvData.pvCopy;
 import java.nio.ByteBuffer;
 
 import org.epics.pvData.misc.BitSet;
+import org.epics.pvData.pv.DeserializableControl;
+import org.epics.pvData.pv.SerializableControl;
 
 
 
@@ -20,15 +22,17 @@ public interface BitSetSerializable {
 	
 	/**
      * Serialize field into given buffer.
-	 * @param buffer serialization buffer.
+	 * @param buffer serialization buffer
+	 * @param flusher flush interface.
      * @param bitSet The BitSet which shows the fields to serialize.
 	 */
-	void serialize(ByteBuffer buffer, BitSet bitSet);
+	void serialize(ByteBuffer buffer, SerializableControl flusher, BitSet bitSet);
 
 	/**
      * Deserialize buffer.
 	 * @param buffer serialization buffer.
+	 * @param control deserialization control instance.
      * @param bitSet The BitSet which shows the fields to deserialize.
 	 */
-     void deserialize(ByteBuffer buffer, BitSet bitSet);
+     void deserialize(ByteBuffer buffer, DeserializableControl control, BitSet bitSet);
 }
