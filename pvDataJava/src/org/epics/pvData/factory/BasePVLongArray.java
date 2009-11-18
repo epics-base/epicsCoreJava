8e9232a13403c1e4f740862c309c5dca46a5e002
msekoranja <unknown>
2009-11-18 12:25:12 +0000

    equals(), hashCode()

--- pvDataJava/src/org/epics/pvData/factory/BasePVLongArray.java
+++ pvDataJava/src/org/epics/pvData/factory/BasePVLongArray.java
@@ -179,4 +179,11 @@ public class BasePVLongArray extends AbstractPVArray implements PVLongArray
 		else
 			return false;
 	}
+    /* (non-Javadoc)
+	 * @see java.lang.Object#hashCode()
+	 */
+	@Override
+	public int hashCode() {
+		return Arrays.hashCode(value);
+	}
 }
