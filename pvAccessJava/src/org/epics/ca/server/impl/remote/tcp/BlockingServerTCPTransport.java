93690228bbac818ce0c4e6830cc60fdf5ca5ae47
mrkraimer <unknown>
2010-05-03 14:45:42 +0000

    This is the new eclipse project name for what was CAJv4

---
+++ pvAccessJava/src/org/epics/ca/server/impl/remote/tcp/BlockingServerTCPTransport.java
@@ -0,0 +1,270 @@
+/*
+ * Copyright (c) 2004 by Cosylab
+ *
+ * The full license specifying the redistribution, modification, usage and other
+ * rights and obligations is included with the distribution of this project in
+ * the file "LICENSE-CAJ". If the license is not included visit Cosylab web site,
+ * <http://www.cosylab.com>.
+ *
+ * THIS SOFTWARE IS PROVIDED AS-IS WITHOUT WARRANTY OF ANY KIND, NOT EVEN THE
+ * IMPLIED WARRANTY OF MERCHANTABILITY. THE AUTHOR OF THIS SOFTWARE, ASSUMES
+ * _NO_ RESPONSIBILITY FOR ANY CONSEQUENCE RESULTING FROM THE USE, MODIFICATION,
+ * OR REDISTRIBUTION OF THIS SOFTWARE.
+ */
+
+package org.epics.ca.server.impl.remote.tcp;
+
+import java.nio.ByteBuffer;
+import java.nio.channels.SocketChannel;
+import java.util.concurrent.atomic.AtomicInteger;
+
+import org.epics.ca.CAConstants;
+import org.epics.ca.impl.remote.ChannelHostingTransport;
+import org.epics.ca.impl.remote.Context;
+import org.epics.ca.impl.remote.IntrospectionRegistry;
+import org.epics.ca.impl.remote.ResponseHandler;
+import org.epics.ca.impl.remote.ServerChannel;
+import org.epics.ca.impl.remote.TransportSendControl;
+import org.epics.ca.impl.remote.TransportSender;
+import org.epics.ca.impl.remote.tcp.BlockingTCPTransport;
+import org.epics.ca.util.IntHashMap;
+import org.epics.pvData.pv.PVField;
+
+/**
+ * Server TCP transport implementation.
+ * @author <a href="mailto:matej.sekoranjaATcosylab.com">Matej Sekoranja</a>
+ * @version $Id$
+ */
+public class BlockingServerTCPTransport extends BlockingTCPTransport implements ChannelHostingTransport, TransportSender {
+
+	/**
+	 * Last SID cache. 
+	 */
+	private AtomicInteger lastChannelSID = new AtomicInteger(0);
+
+	/**
+	 * Channel table (SID -> channel mapping).
+	 */
+	private IntHashMap channels;
+
+	/**
+	 * Introspection registry.
+	 */
+	protected IntrospectionRegistry introspectionRegistry;
+
+	/**
+	 * Server TCP transport constructor.
+	 * @param context context where transport lives in.
+	 * @param channel used socket channel.
+	 * @param responseHandler response handler used to process CA headers.
+	 * @param receiveBufferSize receive buffer size.
+	 */
+	public BlockingServerTCPTransport(Context context, 
+			   SocketChannel channel,
+			   ResponseHandler responseHandler,
+			   int receiveBufferSize) {
+		super(context, channel, responseHandler, receiveBufferSize, CAConstants.CA_DEFAULT_PRIORITY);
+		// NOTE: priority not yet known, default priority is used to register/unregister
+		// TODO implement priorities in Reactor... not that user will change it.. still getPriority() must return "registered" priority!
+		
+		// create introspection registry
+		introspectionRegistry = new IntrospectionRegistry(true);
+
+		final int INITIAL_SIZE = 64;
+		channels = new IntHashMap(INITIAL_SIZE);
+		
+		start();
+	}
+	
+	
+	/**
+	 * @see org.epics.ca.impl.remote.tcp.TCPTransport#internalClose(boolean)
+	 */
+	@Override
+	protected void internalClose(boolean forced) {
+		super.internalClose(forced);
+		destroyAllChannels();
+	}
+
+	/* (non-Javadoc)
+	 * @see org.epics.ca.impl.remote.Transport#getIntrospectionRegistry()
+	 */
+	public IntrospectionRegistry getIntrospectionRegistry() {
+		return introspectionRegistry;
+	}
+
+	/**
+	 * Destroy all channels.
+	 */
+	private void destroyAllChannels() {
+
+		ServerChannel[] channelsArray;
+		synchronized (channels)
+		{
+    		// resource allocation optimization
+    		if (channels.size() == 0)
+    			return;
+
+    		channelsArray = new ServerChannel[channels.size()];
+			channels.toArray(channelsArray);
+
+			channels.clear();
+		}
+
+		context.getLogger().fine("Transport to " + socketAddress + " still has " + channelsArray.length + " channel(s) active and closing...");
+		
+		for (int i = 0; i < channelsArray.length; i++)
+		{
+			try
+			{
+				channelsArray[i].destroy();
+			}
+			catch (Throwable th)
+			{
+				th.printStackTrace();
+			}
+		}
+	}
+
+	/**
+	 * Preallocate new channel SID.
+	 * @return new channel server id (SID).
+	 */
+	public int preallocateChannelSID()
+	{
+		synchronized (channels) {
+			// search first free (theoretically possible loop of death)
+			int sid = lastChannelSID.incrementAndGet();
+			while (channels.containsKey(sid))
+				sid = lastChannelSID.incrementAndGet();
+			return sid;
+		}
+	}
+
+	/**
+	 * De-preallocate new channel SID.
+	 * @param sid preallocated channel SID. 
+	 */
+	public void depreallocateChannelSID(int sid)
+	{
+		// noop
+	}
+
+	/**
+	 * Register a new channel.
+	 * @param sid preallocated channel SID. 
+	 * @param channel channel to register.
+	 */
+	public void registerChannel(int sid, ServerChannel channel)
+	{
+		synchronized (channels) {
+			channels.put(sid, channel);
+		}
+	}
+	
+	/**
+	 * Unregister a new channel (and deallocates its handle).
+	 * @param sid SID
+	 */
+	public void unregisterChannel(int sid)
+	{
+		synchronized (channels) {
+			channels.remove(sid);
+		}
+	}
+
+	/**
+	 * Get channel by its SID.
+	 * @param sid channel SID
+	 * @return channel with given SID, <code>null</code> otherwise
+	 */
+	public ServerChannel getChannel(int sid)
+	{
+		synchronized (channels) {
+			return (ServerChannel)channels.get(sid);
+		}
+	}
+
+	/**
+	 * Get channel count.
+	 * @return channel count.
+	 */
+	public int getChannelCount() 
+	{
+		synchronized (channels) {
+			return channels.size();
+		}
+	}
+
+	/* (non-Javadoc)
+	 * @see org.epics.ca.core.ChannelHostingTransport#getSecurityToken()
+	 */
+	public PVField getSecurityToken() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	/* (non-Javadoc)
+	 * @see org.epics.ca.impl.remote.TransportSender#lock()
+	 */
+	@Override
+	public void lock() {
+		// noop
+	}
+
+	/* (non-Javadoc)
+	 * @see org.epics.ca.impl.remote.TransportSender#unlock()
+	 */
+	@Override
+	public void unlock() {
+		// noop
+	}
+
+	/**
+	 * CA connection validation request.
+	 * A server sends a validate connection message when it receives a new connection.
+	 * The message indicates that the server is ready to receive requests; the client must 
+	 * not send any messages on the connection until it has received the validate connection message
+	 * from the server. No reply to the message is expected by the server. 
+	 * The purpose of the validate connection message is two-fold: 
+	 * It informs the client of the protocol version supported by the server.
+	 * It prevents the client from writing a request message to its local transport 
+	 * buffers until after the server has acknowledged that it can actually process the 
+	 * request. This avoids a race condition caused by the server's TCP/IP stack 
+	 * accepting connections in its backlog while the server is in the process of shutting down:
+	 * if the client were to send a request in this situation, the request 
+	 * would be lost but the client could not safely re-issue the request because that 
+	 * might violate at-most-once semantics. 
+	 * The validate connection message guarantees that a server is not in the middle 
+	 * of shutting down when the server's TCP/IP stack accepts an incoming connection
+	 * and so avoids the race condition. 
+	 * @see org.epics.ca.impl.remote.TransportSender#send(java.nio.ByteBuffer, org.epics.ca.impl.remote.TransportSendControl)
+	 */
+	@Override
+	public void send(ByteBuffer buffer, TransportSendControl control) {
+		//
+		// send verification message
+		//
+		
+		control.startMessage((byte)1, 2*Integer.SIZE/Byte.SIZE);
+		
+		// receive buffer size
+		buffer.putInt(getReceiveBufferSize());
+			
+		// socket receive buffer size
+		buffer.putInt(getSocketReceiveBufferSize());
+		
+		// send immediately
+		control.flush(true);
+	}
+
+	/**
+	 * Verify transport. Server side is self-verified.
+	 */
+	public void verify() 
+	{
+		enqueueSendRequest(this);
+		verified();
+	}
+	
+}
---
+++ pvAccessJava/src/org/epics/ca/server/impl/remote/tcp/BlockingTCPAcceptor.java
@@ -0,0 +1,238 @@
+/*
+ * Copyright (c) 2004 by Cosylab
+ *
+ * The full license specifying the redistribution, modification, usage and other
+ * rights and obligations is included with the distribution of this project in
+ * the file "LICENSE-CAJ". If the license is not included visit Cosylab web site,
+ * <http://www.cosylab.com>.
+ *
+ * THIS SOFTWARE IS PROVIDED AS-IS WITHOUT WARRANTY OF ANY KIND, NOT EVEN THE
+ * IMPLIED WARRANTY OF MERCHANTABILITY. THE AUTHOR OF THIS SOFTWARE, ASSUMES
+ * _NO_ RESPONSIBILITY FOR ANY CONSEQUENCE RESULTING FROM THE USE, MODIFICATION,
+ * OR REDISTRIBUTION OF THIS SOFTWARE.
+ */
+
+package org.epics.ca.server.impl.remote.tcp;
+
+import java.io.IOException;
+import java.net.BindException;
+import java.net.InetSocketAddress;
+import java.net.SocketAddress;
+import java.nio.channels.ServerSocketChannel;
+import java.nio.channels.SocketChannel;
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.logging.Level;
+
+import org.epics.ca.CAException;
+import org.epics.ca.impl.remote.Context;
+import org.epics.ca.server.impl.remote.ServerContextImpl;
+import org.epics.ca.server.impl.remote.ServerResponseHandler;
+
+/**
+ * Channel Access Server TCP acceptor.
+ * @author <a href="mailto:matej.sekoranjaATcosylab.com">Matej Sekoranja</a>
+ * @version $Id$
+ */
+public class BlockingTCPAcceptor {
+
+	/**
+	 * Context instance.
+	 */
+	private Context context;
+	
+	/**
+	 * Bind server socket address.
+	 */
+	private InetSocketAddress bindAddress = null;
+
+	/**
+	 * Server socket channel.
+	 */
+	private ServerSocketChannel serverSocketChannel = null;
+
+	/**
+	 * Receive buffer size.
+	 */
+	private int receiveBufferSize;
+	
+	/**
+	 * Destroyed flag.
+	 */
+	private AtomicBoolean destroyed = new AtomicBoolean(false);
+
+	/**
+	 * @param context
+	 * @param port
+	 * @param receiveBufferSize
+	 * @throws CAException
+	 */
+	public BlockingTCPAcceptor(Context context, int port, int receiveBufferSize) throws CAException {
+		this.context = context;
+		this.receiveBufferSize = receiveBufferSize;
+		
+		initialize(port);
+	}
+
+	/**
+	 * Handle IO events.
+	 */
+	public void handleEvents() {
+		// rise level if port is assigned dynamically
+		context.getLogger().finer("Accepting connections at " + bindAddress + ".");
+
+		while (!destroyed.get() && serverSocketChannel.isOpen())
+		{
+			try
+			{
+				// this will block
+				SocketChannel socket = serverSocketChannel.accept();
+				
+				SocketAddress address = socket.socket().getRemoteSocketAddress();
+				context.getLogger().finer("Accepted connection from CA client: " + address);
+				
+				// enable TCP_NODELAY (disable Nagle's algorithm)
+				socket.socket().setTcpNoDelay(true);
+				
+				// enable TCP_KEEPALIVE
+				socket.socket().setKeepAlive(true);
+			
+				// TODO tune buffer sizes?!
+				//socket.socket().setReceiveBufferSize();
+				//socket.socket().setSendBufferSize();
+				
+				// create transport
+				// each transport should have its own response handler since it is not "shareable"
+				final BlockingServerTCPTransport transport = new BlockingServerTCPTransport(context, socket, new ServerResponseHandler((ServerContextImpl)context), receiveBufferSize);
+	
+				// validate connection
+				if (!validateConnection(transport, address))
+				{
+					transport.close(true);
+					context.getLogger().finer("Connection to CA client " + address + " failed to be validated, closing it.");
+					return;
+				}
+				
+				context.getLogger().finer("Serving to CA client: " + address);
+	
+			} 
+			catch (Throwable th)
+			{
+				// TODO remove !!!
+				th.printStackTrace();
+			}
+		}
+	}
+
+
+	/**
+	 * Validate connection by sending a validation message request.
+	 * @return <code>true</code> on success.
+	 */
+	private boolean validateConnection(BlockingServerTCPTransport transport, SocketAddress address)
+	{
+		try {
+			transport.verify();
+			return true;
+		}
+		catch (Throwable th) {
+			context.getLogger().log(Level.FINEST, "Validation of " + address + " failed.", th);
+			return false;
+		}
+	}
+	
+	/**
+	 * Initialize connection acception. 
+	 * @return port where server is listening
+	 */
+	private int initialize(int port) throws CAException
+	{
+		// specified bind address
+		bindAddress = new InetSocketAddress(port);
+
+		int tryCount = 0;
+		while (true)
+		{
+			tryCount++;
+			
+			try
+			{
+				context.getLogger().finer("Creating acceptor to " + bindAddress + ".");
+	
+				serverSocketChannel = ServerSocketChannel.open();
+				serverSocketChannel.socket().bind(bindAddress);
+				serverSocketChannel.configureBlocking(true);
+				
+				// update bind address, if dynamically port selection was used
+				if (bindAddress.getPort() == 0)
+				{
+					bindAddress = new InetSocketAddress(serverSocketChannel.socket().getLocalPort());
+					context.getLogger().info("Using dynamically assigned TCP port " + bindAddress.getPort() + ".");
+				}
+
+				new Thread(new Runnable() {
+					
+					@Override
+					public void run() {
+						handleEvents();
+					}
+				}, "TCP-acceptor").start();
+				
+				// all OK, return
+				return bindAddress.getPort();
+			}
+			catch (BindException be)
+			{
+				// failed to bind to specified bind address,
+				// try to get port dynamically, but only once
+				if (tryCount == 1)
+				{
+					context.getLogger().info("Configured TCP port " + port + " is unavailable, trying to assign it dynamically.");
+					bindAddress = new InetSocketAddress(0);
+				}
+				else
+				{
+					throw new CAException("Failed to create acceptor to " + bindAddress, be);
+				}				
+			}
+			catch (Throwable th)
+			{
+				throw new CAException("Failed to create acceptor to " + bindAddress, th);
+			}
+		}
+		
+	}
+	
+	/**
+	 * Bind socket address.
+	 * @return bind socket address, <code>null</code> if not binded.
+	 */
+	public InetSocketAddress getBindAddress() 
+	{
+		return bindAddress;
+		/*
+		return (serverSocketChannel != null) ? 
+				serverSocketChannel.socket().getInetAddress() : null;
+		*/
+	}
+	
+	/**
+	 * Destroy acceptor (stop listening).
+	 */
+	public void destroy()
+	{
+		if (destroyed.getAndSet(true))
+			return;
+		
+		if (serverSocketChannel != null)
+		{
+			context.getLogger().finer("Stopped accepting connections at " + bindAddress + ".");
+			try {
+				serverSocketChannel.socket().close();
+			} catch (IOException e) {
+				// just log
+				context.getLogger().log(Level.FINE, "Failed to close acceptor socket at " + bindAddress + ".", e);
+			}
+		}
+	}
+	
+}
