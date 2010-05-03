df43b95163dd099f83d70f8a4048b6d3cae29a54
mrkraimer <unknown>
2010-05-03 11:04:55 +0000

    merge copyMonitiorChanges to HEAD

--- pvDataJava/src/org/epics/pvData/pv/FieldCreate.java
+++ pvDataJava/src/org/epics/pvData/pv/FieldCreate.java
@@ -22,22 +22,36 @@ public interface FieldCreate {
      * Create a <i>ScalarField</i>.
      * @param fieldName The field name.
      * @param scalarType The scalar type.
-     * @return a <i>Field</i> interface for the newly created object.
+     * @return a <i>Scalar</i> interface for the newly created object.
      * @throws An <i>IllegalArgumentException</i> if an illegal type is specified.
      */
     Scalar createScalar(String fieldName,ScalarType scalarType);
     /**
+     * Create a <i>Scalar</i> with scalarType pvStructure;
+     * @param fieldName The field name.
+     * @param structure The introspection interface that describes the subfields.
+     * @return a <i>Scalar</i> interface for the newly created object.
+     */
+    StructureScalar createStructureScalar(String fieldName,Structure structure);
+    /**
      * Create an <i>Array</i> field.
      * @param fieldName The field name
-     * @param elementType The <i>Type</i> for array elements
+     * @param elementType The <i>scalarType</i> for array elements
      * @return An <i>Array</i> Interface for the newly created object.
      */
     Array createArray(String fieldName,ScalarType elementType);
     /**
+     * Create an <i>Array</i> field that is has element type <i>Structure</i>
+     * @param fieldName The field name
+     * @param fields The <i>Structure</i> for each array element.
+     * @return An <i>Array</i> Interface for the newly created object.
+     */
+    StructureArray createStructureArray(String fieldName,Structure elementStructure);
+    /**
      * Create a <i>Structure</i> field.
      * @param fieldName The field name
-     * @param field The array of <i>Field</i>s for the structure.
+     * @param fields The array of <i>Field</i>s for the structure.
      * @return a <i>Structure</i> interface for the newly created object.
      */
-    Structure createStructure(String fieldName, Field[] field);
+    Structure createStructure(String fieldName, Field[] fields);
 }
