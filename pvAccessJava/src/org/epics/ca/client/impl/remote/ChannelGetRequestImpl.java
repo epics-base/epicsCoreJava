c9422838efb9dd6533c8406b823e49a080ced96f
mrkraimer <unknown>
2010-05-03 14:45:40 +0000

    This is the new eclipse project name for what was CAJv4

---
+++ pvAccessJava/src/org/epics/ca/client/impl/remote/ChannelGetRequestImpl.java
@@ -0,0 +1,180 @@
+/*
+ * Copyright (c) 2004 by Cosylab
+ *
+ * The full license specifying the redistribution, modification, usage and other
+ * rights and obligations is included with the distribution of this project in
+ * the file "LICENSE-CAJ". If the license is not included visit Cosylab web site,
+ * <http://www.cosylab.com>.
+ *
+ * THIS SOFTWARE IS PROVIDED AS-IS WITHOUT WARRANTY OF ANY KIND, NOT EVEN THE
+ * IMPLIED WARRANTY OF MERCHANTABILITY. THE AUTHOR OF THIS SOFTWARE, ASSUMES
+ * _NO_ RESPONSIBILITY FOR ANY CONSEQUENCE RESULTING FROM THE USE, MODIFICATION,
+ * OR REDISTRIBUTION OF THIS SOFTWARE.
+ */
+
+package org.epics.ca.client.impl.remote;
+
+import java.nio.ByteBuffer;
+
+import org.epics.ca.CAException;
+import org.epics.ca.client.ChannelGet;
+import org.epics.ca.client.ChannelGetRequester;
+import org.epics.ca.impl.remote.QoS;
+import org.epics.ca.impl.remote.Transport;
+import org.epics.ca.impl.remote.TransportSendControl;
+import org.epics.pvData.misc.BitSet;
+import org.epics.pvData.pv.PVStructure;
+import org.epics.pvData.pv.Status;
+import org.epics.pvData.pv.Status.StatusType;
+
+/**
+ * CA get request.
+ * @author <a href="mailto:matej.sekoranjaATcosylab.com">Matej Sekoranja</a>
+ * @version $Id$
+ */
+public class ChannelGetRequestImpl extends BaseRequestImpl implements ChannelGet {
+
+    /**
+	 * Response callback listener.
+	 */
+	protected final ChannelGetRequester callback;
+
+	protected final PVStructure pvRequest;
+
+	protected volatile PVStructure data = null;
+	protected volatile BitSet bitSet = null;
+	
+	public ChannelGetRequestImpl(ChannelImpl channel, ChannelGetRequester callback,
+            PVStructure pvRequest)
+	{
+		super(channel, callback);
+		
+		if (callback == null)
+			throw new IllegalArgumentException("null requester");
+
+		if (pvRequest == null)
+			throw new IllegalArgumentException("null pvRequest");
+		
+		this.callback = callback;
+		
+		this.pvRequest = pvRequest;
+		
+		// TODO immediate get, i.e. get data with init message
+		// TODO one-time get, i.e. immediate get + lastRequest 
+
+		// subscribe
+		try {
+			resubscribeSubscription(channel.checkAndGetTransport());
+		} catch (IllegalStateException ise) {
+			callback.channelGetConnect(channelNotConnected, null, null, null);
+		} catch (CAException caex) {
+			callback.channelGetConnect(statusCreate.createStatus(StatusType.ERROR, "failed to sent message over network", caex), null, null, null);
+		}
+	}
+
+	/* (non-Javadoc)
+	 * @see org.epics.ca.impl.remote.TransportSender#send(java.nio.ByteBuffer, org.epics.ca.impl.remote.TransportSendControl)
+	 */
+	@Override
+	public void send(ByteBuffer buffer, TransportSendControl control) {
+		final int pendingRequest = getPendingRequest();
+		if (pendingRequest < 0)
+		{
+			super.send(buffer, control);
+			return;
+		}
+		
+		control.startMessage((byte)10, 2*Integer.SIZE/Byte.SIZE+1);
+		buffer.putInt(channel.getServerChannelID());
+		buffer.putInt(ioid);
+		buffer.put((byte)pendingRequest);
+		
+		if (QoS.INIT.isSet(pendingRequest))
+		{
+			// pvRequest
+			channel.getTransport().getIntrospectionRegistry().serializePVRequest(buffer, control, pvRequest);
+		}
+		
+		stopRequest();
+	}
+
+	/* (non-Javadoc)
+	 * @see org.epics.ca.client.impl.remote.channelAccess.BaseRequestImpl#destroyResponse(org.epics.ca.core.Transport, byte, java.nio.ByteBuffer, byte, org.epics.pvData.pv.Status)
+	 */
+	@Override
+	void destroyResponse(Transport transport, byte version, ByteBuffer payloadBuffer, byte qos, Status status) {
+		// data available
+		if (QoS.GET.isSet(qos))
+			normalResponse(transport, version, payloadBuffer,qos, status);
+	}
+
+	/* (non-Javadoc)
+	 * @see org.epics.ca.client.impl.remote.channelAccess.BaseRequestImpl#initResponse(org.epics.ca.core.Transport, byte, java.nio.ByteBuffer, byte, org.epics.pvData.pv.Status)
+	 */
+	@Override
+	void initResponse(Transport transport, byte version, ByteBuffer payloadBuffer, byte qos, Status status) {
+		if (!status.isSuccess())
+		{
+			callback.channelGetConnect(status, this, null, null);
+			return;
+		}
+		
+		// create data and its bitSet
+		data = transport.getIntrospectionRegistry().deserializeStructureAndCreatePVStructure(payloadBuffer, transport);
+		bitSet = new BitSet(data.getNumberFields());
+
+		// notify
+		callback.channelGetConnect(okStatus, this, data, bitSet);
+	}
+
+	/* (non-Javadoc)
+	 * @see org.epics.ca.client.impl.remote.channelAccess.BaseRequestImpl#normalResponse(org.epics.ca.core.Transport, byte, java.nio.ByteBuffer, byte, org.epics.pvData.pv.Status)
+	 */
+	@Override
+	void normalResponse(Transport transport, byte version, ByteBuffer payloadBuffer, byte qos, Status status) {
+		if (!status.isSuccess())
+		{
+			callback.getDone(status);
+			return;
+		}
+		
+		// deserialize bitSet and data
+		bitSet.deserialize(payloadBuffer, transport);
+		data.deserialize(payloadBuffer, transport, bitSet);
+		
+		callback.getDone(okStatus);
+	}
+
+	/* (non-Javadoc)
+	 * @see org.epics.ca.client.ChannelGet#get(boolean)
+	 */
+	@Override
+	public synchronized void get(boolean lastRequest) {
+		if (destroyed) {
+			callback.getDone(destroyedStatus);
+			return;
+		}
+
+		if (!startRequest(lastRequest ? QoS.DESTROY.getMaskValue() | QoS.GET.getMaskValue() : QoS.DEFAULT.getMaskValue())) {
+			callback.getDone(otherRequestPendingStatus);
+			return;
+		}
+		
+		try {
+			channel.checkAndGetTransport().enqueueSendRequest(this);
+		} catch (IllegalStateException ise) {
+			callback.getDone(channelNotConnected);
+		}
+	}
+	
+	/* Called on server restart...
+	 * @see org.epics.ca.core.SubscriptionRequest#resubscribeSubscription(org.epics.ca.core.Transport)
+	 */
+	@Override
+	public final void resubscribeSubscription(Transport transport) throws CAException {
+		int qos = QoS.INIT.getMaskValue();
+		startRequest(qos);
+		transport.enqueueSendRequest(this);
+	}
+	
+}
