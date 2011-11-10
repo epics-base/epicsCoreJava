b2c598015a5b0d59e176ee35f1f27d5410dabb04
Matej Sekoranja <unknown>
2011-11-10 15:21:31 +0100

    more tests

--- pvAccessJava/test/org/epics/ca/server/test/TestChannelProviderImpl.java
+++ pvAccessJava/test/org/epics/ca/server/test/TestChannelProviderImpl.java
@@ -15,6 +15,8 @@
 package org.epics.ca.server.test;
 
 import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 
 import org.epics.ca.client.AccessRights;
 import org.epics.ca.client.Channel;
@@ -35,12 +37,19 @@ import org.epics.ca.client.ChannelRPC;
 import org.epics.ca.client.ChannelRPCRequester;
 import org.epics.ca.client.ChannelRequester;
 import org.epics.ca.client.GetFieldRequester;
+import org.epics.pvData.factory.FieldFactory;
+import org.epics.pvData.factory.PVDataFactory;
 import org.epics.pvData.factory.StatusFactory;
+import org.epics.pvData.misc.BitSet;
 import org.epics.pvData.monitor.Monitor;
 import org.epics.pvData.monitor.MonitorRequester;
+import org.epics.pvData.pv.Field;
+import org.epics.pvData.pv.FieldCreate;
 import org.epics.pvData.pv.MessageType;
+import org.epics.pvData.pv.PVDataCreate;
 import org.epics.pvData.pv.PVField;
 import org.epics.pvData.pv.PVStructure;
+import org.epics.pvData.pv.ScalarType;
 import org.epics.pvData.pv.Status;
 import org.epics.pvData.pv.Status.StatusType;
 
@@ -50,17 +59,108 @@ import org.epics.pvData.pv.Status.StatusType;
  */
 public class TestChannelProviderImpl implements ChannelProvider
 {
-	private static final Status okStatus = StatusFactory.getStatusCreate().getStatusOK();
+    private static final FieldCreate fieldCreate = FieldFactory.getFieldCreate();
+    private static final PVDataCreate pvDataCreate = PVDataFactory.getPVDataCreate();
+
+    private static final Status okStatus = StatusFactory.getStatusCreate().getStatusOK();
+	private static final Status fieldDoesNotExistStatus =
+		StatusFactory.getStatusCreate().createStatus(StatusType.ERROR, "field does not exist", null);
+	private static final Status destroyedStatus =
+		StatusFactory.getStatusCreate().createStatus(StatusType.ERROR, "channel destroyed", null);
 
 	class TestChannelImpl implements Channel
 	{
+		
+		class TestChannelGetImpl implements ChannelGet
+		{
+			private final ChannelGetRequester channelGetRequester;
+			private final Lock mutex = new ReentrantLock();
+			private final AtomicBoolean destroyed = new AtomicBoolean();
+			private final PVStructure pvGetStructure;
+			private final BitSet bitSet;
+			
+			public TestChannelGetImpl(ChannelGetRequester channelGetRequester, PVStructure pvRequest)
+			{
+				this.channelGetRequester = channelGetRequester;
+				
+				pvGetStructure = pvStructure;
+				bitSet = new BitSet(pvGetStructure.getNumberFields());
+					
+				channelGetRequester.channelGetConnect(okStatus, this, pvGetStructure, bitSet);
+			}
+
+			@Override
+			public void lock() {
+				mutex.lock();
+			}
+
+			@Override
+			public void unlock() {
+				mutex.unlock();
+			}
+
+			@Override
+			public void get(boolean lastRequest) {
+				if (destroyed.get())
+				{
+					channelGetRequester.getDone(destroyedStatus);
+					return;
+				}
+				
+				channelGetRequester.getDone(okStatus);
+
+				if (lastRequest)
+					destroy();
+			}
+
+			@Override
+			public void destroy() {
+				if (destroyed.getAndSet(true))
+					return;
+			}
+
+		}
+		
+		
+		
+		
 		private final String channelName;
 		private final ChannelRequester channelRequester;
+		private final PVStructure pvStructure;
 		
-		TestChannelImpl(String channelName, ChannelRequester channelRequester)
+		TestChannelImpl(String channelName, ChannelRequester channelRequester, ScalarType type)
 		{
 			this.channelName = channelName;
 			this.channelRequester = channelRequester;
+			
+			
+			
+			
+			PVStructure timeStampStructure;
+			{
+		        Field[] fields = new Field[3];
+		        fields[0] = fieldCreate.createScalar("secondsPastEpoch", ScalarType.pvLong);
+		        fields[1] = fieldCreate.createScalar("nanoSeconds", ScalarType.pvInt);
+		        fields[2] = fieldCreate.createScalar("userTag", ScalarType.pvInt);
+		        timeStampStructure = pvDataCreate.createPVStructure(null, "timeStamp", fields);
+			}
+		
+			PVStructure alarmStructure;
+			{
+		        Field[] fields = new Field[3];
+		        fields[0] = fieldCreate.createScalar("severity", ScalarType.pvInt);
+		        fields[1] = fieldCreate.createScalar("status", ScalarType.pvInt);
+		        fields[2] = fieldCreate.createScalar("message", ScalarType.pvString);
+		        alarmStructure = pvDataCreate.createPVStructure(null, "alarm", fields);
+			}
+			
+	        Field[] fields = new Field[3];
+	        fields[0] = FieldFactory.getFieldCreate().createScalar("value", type);
+	        fields[1] = timeStampStructure.getField();
+	        fields[2] = alarmStructure.getField();
+	        
+	        pvStructure = pvDataCreate.createPVStructure(null, channelName, fields);
+			
 			
 			setConnectionState(ConnectionState.CONNECTED);
 		}
@@ -123,11 +223,28 @@ public class TestChannelProviderImpl implements ChannelProvider
 			return channelRequester;
 		}
 
-
 		@Override
 		public void getField(GetFieldRequester requester, String subField) {
-			// TODO Auto-generated method stub
 			
+			if (requester == null)
+				throw new IllegalArgumentException("requester");
+			
+			if (destroyed.get())
+			{
+				requester.getDone(destroyedStatus, null);
+				return;
+			}
+			
+			Field field;
+			if (subField == null)
+				field = pvStructure.getStructure();
+			else
+				field = pvStructure.getStructure().getField(subField);
+			
+			if (field != null)
+				requester.getDone(okStatus, field);
+			else
+				requester.getDone(fieldDoesNotExistStatus, null);
 		}
 
 		@Override
@@ -147,8 +264,14 @@ public class TestChannelProviderImpl implements ChannelProvider
 		@Override
 		public ChannelGet createChannelGet(
 				ChannelGetRequester channelGetRequester, PVStructure pvRequest) {
-			// TODO Auto-generated method stub
-			return null;
+			
+			if (channelGetRequester == null)
+				throw new IllegalArgumentException("channelGetRequester");
+			
+			if (pvRequest == null)
+				throw new IllegalArgumentException("pvRequest");
+
+			return new TestChannelGetImpl(channelGetRequester, pvRequest);
 		}
 
 		@Override
@@ -225,6 +348,18 @@ public class TestChannelProviderImpl implements ChannelProvider
 			channelName.equals("valueOnly");
 	}
 
+	private ScalarType getType(String channelName)
+	{
+		if (channelName.equals("counter") ||
+			channelName.equals("simpleCounter"))
+			return ScalarType.pvInt;
+		
+		if (channelName.equals("valueOnly"))
+				return ScalarType.pvDouble;
+		
+		return ScalarType.pvDouble;
+	}
+
 	@Override
 	public ChannelFind channelFind(String channelName,
 			ChannelFindRequester channelFindRequester) {
@@ -262,7 +397,7 @@ public class TestChannelProviderImpl implements ChannelProvider
 			throw new IllegalArgumentException("priority out of range");
 			
 		Channel channel = isSupported(channelName) ?
-				new TestChannelImpl(channelName, channelRequester) :
+				new TestChannelImpl(channelName, channelRequester, getType(channelName)) :
 				null;
 		
 		Status status = (channel == null) ? channelNotFoundStatus : okStatus;
