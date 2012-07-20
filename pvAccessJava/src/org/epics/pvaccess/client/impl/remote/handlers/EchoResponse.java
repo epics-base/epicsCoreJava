a3fa8a9be9494cdeb39c604892138115256b9f92
Matej Sekoranja <unknown>
2012-07-20 08:50:00 +0200

    connection handling fixed

---
+++ pvAccessJava/src/org/epics/pvaccess/client/impl/remote/handlers/EchoResponse.java
@@ -0,0 +1,45 @@
+/*
+ * Copyright (c) 2004 by Cosylab
+ *
+ * The full license specifying the redistribution, modification, usage and other
+ * rights and obligations is included with the distribution of this project in
+ * the file "LICENSE-CAJ". If the license is not included visit Cosylab web site,
+ * <http://www.cosylab.com>.
+ *
+ * THIS SOFTWARE IS PROVIDED AS-IS WITHOUT WARRANTY OF ANY KIND, NOT EVEN THE
+ * IMPLIED WARRANTY OF MERCHANTABILITY. THE AUTHOR OF THIS SOFTWARE, ASSUMES
+ * _NO_ RESPONSIBILITY FOR ANY CONSEQUENCE RESULTING FROM THE USE, MODIFICATION,
+ * OR REDISTRIBUTION OF THIS SOFTWARE.
+ */
+
+package org.epics.pvaccess.client.impl.remote.handlers;
+
+import java.net.InetSocketAddress;
+import java.nio.ByteBuffer;
+
+import org.epics.pvaccess.client.impl.remote.ClientContextImpl;
+import org.epics.pvaccess.impl.remote.Transport;
+
+/**
+ * NOOP response.
+ * @author <a href="mailto:matej.sekoranjaATcosylab.com">Matej Sekoranja</a>
+ * @version $Id$
+ */
+public class EchoResponse extends AbstractClientResponseHandler {
+
+	/**
+	 * @param context
+	 */
+	public EchoResponse(ClientContextImpl context) {
+		super(context, "Echo");
+	}
+
+	/* (non-Javadoc)
+	 * @see org.epics.pvaccess.impl.remote.AbstractResponseHandler#handleResponse(java.net.InetSocketAddress, org.epics.pvaccess.core.Transport, byte, byte, int, java.nio.ByteBuffer)
+	 */
+	@Override
+	public void handleResponse(InetSocketAddress responseFrom, Transport transport, byte version, byte command, int payloadSize, ByteBuffer payloadBuffer) {
+		super.handleResponse(responseFrom, transport, version, command, payloadSize, payloadBuffer);
+		transport.aliveNotification();
+	}
+}
