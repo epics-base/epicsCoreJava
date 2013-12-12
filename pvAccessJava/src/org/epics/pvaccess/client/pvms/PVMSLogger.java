3d24590c6186656f1fd5b0d4d79619b9db2ecd75
Matej Sekoranja <unknown>
2013-12-12 21:41:46 +0100

    added Java Logging API LoggingHandler and simple detector implementation using pvMS as example

---
+++ pvAccessJava/src/org/epics/pvaccess/client/pvms/PVMSLogger.java
@@ -0,0 +1,290 @@
+package org.epics.pvaccess.client.pvms;
+
+import java.io.IOException;
+import java.net.InetAddress;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+import java.util.UUID;
+import java.util.concurrent.atomic.AtomicLong;
+import java.util.logging.Handler;
+import java.util.logging.Level;
+import java.util.logging.LogManager;
+import java.util.logging.LogRecord;
+import java.util.logging.Logger;
+
+import org.epics.pvdata.factory.FieldFactory;
+import org.epics.pvdata.factory.PVDataFactory;
+import org.epics.pvdata.pv.PVInt;
+import org.epics.pvdata.pv.PVLong;
+import org.epics.pvdata.pv.PVString;
+import org.epics.pvdata.pv.PVStructure;
+import org.epics.pvdata.pv.PVStructureArray;
+import org.epics.pvdata.pv.ScalarType;
+import org.epics.pvdata.pv.Structure;
+
+public class PVMSLogger {
+
+	public static final Structure timeStructure =
+		FieldFactory.getFieldCreate().createFieldBuilder().
+			setId("time_t").
+			add("secsPastEpoch", ScalarType.pvLong).
+			add("nanoseconds", ScalarType.pvInt).
+			add("userTag", ScalarType.pvInt).
+			createStructure();
+
+	public static final Structure nameValue =
+		FieldFactory.getFieldCreate().createFieldBuilder().
+			add("name", ScalarType.pvString).
+			add("value", ScalarType.pvString).
+			createStructure();
+		
+	public static final Structure logStructure =
+		FieldFactory.getFieldCreate().createFieldBuilder().
+			setId("uri:ev4:nt/2012/pwd/NTLogRecord").
+			
+			// used to group all the logs from one "session"
+			add("uuid_msb", ScalarType.pvLong).
+			add("uuid_lsb", ScalarType.pvLong).
+			
+			// sequence number of a log withing session
+			add("seqNo", ScalarType.pvLong).
+
+			add("time", timeStructure).
+			
+			// TBD, for now I just pass java level
+			add("level", ScalarType.pvInt).
+			
+			add("loggerName", ScalarType.pvString).
+
+			add("message", ScalarType.pvString).	
+			add("host", ScalarType.pvString).	
+			add("process", ScalarType.pvString).
+			/*
+			// the following data can be sent via properties
+			// they are generally useful only in case of errors for debugging,
+			// however stack trace carries most of the information (excepting threadID) 
+			add("file", ScalarType.pvString).
+			add("line", ScalarType.pvInt). 
+			add("method", ScalarType.pvString). 
+			add("source", ScalarType.pvString).	// java.util.Timer / ::std::vector
+			add("threadID", ScalarType.pvInt).	
+			*/
+			add("stackTrace", ScalarType.pvString).	
+			addArray("properties", nameValue).	
+			
+			createStructure();
+
+	public static class PVMSLogingHandler extends Handler
+	{
+		
+		private final PVStructure log =
+			PVDataFactory.getPVDataCreate().createPVStructure(logStructure);
+
+		//private final PVLong uuid_msb = log.getSubField(PVLong.class, "uuid_msb");
+		//private final PVLong uuid_lsb = log.getSubField(PVLong.class, "uuid_lsb");
+
+		private final PVLong seqNo = log.getSubField(PVLong.class, "seqNo");
+		
+		private final PVStructure time = log.getSubField(PVStructure.class, "time");
+		private final PVLong secsPastEpoch = time.getSubField(PVLong.class, "secsPastEpoch");
+		private final PVInt nanoseconds = time.getSubField(PVInt.class, "nanoseconds");
+		
+		private final PVInt level = log.getSubField(PVInt.class, "level");
+		private final PVString loggerName = log.getSubField(PVString.class, "loggerName");
+
+		private final PVString message = log.getSubField(PVString.class, "message");
+		
+		// no need to send the following two fields every time,
+		// they are the same for all the logs with same session uuid
+		//private final PVString host = log.getSubField(PVString.class, "host");
+		//private final PVString process = log.getSubField(PVString.class, "process");
+
+		/*
+		private final PVString file = log.getSubField(PVString.class, "file");
+		private final PVInt line = log.getSubField(PVInt.class, "line");
+		private final PVString method = log.getSubField(PVString.class, "method");
+		private final PVString source = log.getSubField(PVString.class, "source");
+		private final PVInt threadID = log.getSubField(PVInt.class, "threadID");
+		 */
+		
+		private final PVString stackTrace = log.getSubField(PVString.class, "stackTrace");
+
+		private final PVStructureArray properties = log.getSubField(PVStructureArray.class, "properties");
+		
+		private final AtomicLong sequenceNoCounter = new AtomicLong(0);
+
+		private final PVMSPublisher publisher;
+		
+		public PVMSLogingHandler(InetAddress sendAddress, int port) throws IOException
+		{
+			publisher = new PVMSPublisher(sendAddress, port);
+			
+			UUID uuid = UUID.randomUUID();
+			log.getSubField(PVLong.class, "uuid_msb").put(uuid.getMostSignificantBits());
+			log.getSubField(PVLong.class, "uuid_lsb").put(uuid.getLeastSignificantBits());
+			
+			time.getSubField(PVInt.class, "userTag").put(0);
+			log.getSubField(PVString.class, "host").put(getHostName());
+			
+			// work only on Oracle (gives main class or Jar), but better than nothing
+			String cmdLine = System.getProperty("sun.java.command", "");
+			int pos = cmdLine.indexOf(' ');
+			if (pos > 0)
+				cmdLine = cmdLine.substring(0, pos);
+			log.getSubField(PVString.class, "process").put(cmdLine);
+		}
+		
+		@SuppressWarnings({ "rawtypes", "unchecked" })
+		@Override
+		public synchronized void publish(LogRecord record)
+		{
+			if (isLoggable(record))
+			{
+				// reuse existing log structure
+	
+				seqNo.put(sequenceNoCounter.getAndIncrement());
+			
+				long t = record.getMillis();
+				secsPastEpoch.put(t/1000);
+		        nanoseconds.put((int)((t % 1000)*1000000));
+				
+		        // TODO do mapping to standardized NTLogRecord levels
+				level.put(record.getLevel().intValue());	
+				
+				loggerName.put(record.getLoggerName());
+				message.put(record.getMessage());
+
+				/*
+				// extracting the following information from Thread.currentThread().getStackTrace() is 
+				// very slow, disabled
+				file.put();
+				line.put();
+				method.put();
+				source.put();
+				
+				threadID.put(record.getThreadID());
+				 */
+				
+				Throwable th = record.getThrown();
+				if (th != null)
+					stackTrace.put(th.toString());
+				else
+					stackTrace.put(null);
+	
+				Object[] parameters = record.getParameters();
+				if (parameters != null && parameters.length > 0)
+				{
+					// TODO suboptimal, reuse() when new array support is there
+					ArrayList<PVStructure> params = new ArrayList<PVStructure>();
+					for (Object o : parameters)
+						if (o instanceof Map)
+						{
+							Map map = (Map)o;
+							Set<Map.Entry> entries = map.entrySet();
+							for (Map.Entry entry : entries)
+							{
+								PVStructure nv = PVDataFactory.getPVDataCreate().createPVStructure(nameValue);
+								nv.getSubField(PVString.class, "name").put((String)(entry.getKey()));
+								nv.getSubField(PVString.class, "value").put((String)(entry.getValue()));
+								params.add(nv);
+							}
+						}
+					properties.setLength(params.size());
+					properties.put(0, params.size(), params.toArray(new PVStructure[params.size()]), 0);
+				}
+				else
+					properties.setLength(0);
+				
+				try {
+					publisher.publishData("LOG", log);
+				} catch (IOException e) {
+					// noop
+				}
+			}
+		}
+
+		@Override
+		public void flush() {
+			// noop
+		}
+
+		@Override
+		public void close() throws SecurityException {
+			publisher.destroy();
+		}
+
+		private static String hostName = null;
+		private static final String HOSTNAME_KEY = "HOSTNAME";
+		
+		private static synchronized String getHostName() 
+		{
+			if (hostName == null)
+			{
+				// default fallback
+				hostName = "localhost";
+				
+				try {
+					InetAddress localAddress = InetAddress.getLocalHost();
+					hostName = localAddress.getHostName();
+				} catch (Throwable uhe) {	// not only UnknownHostException
+					// try with environment variable
+					try {
+						String envHN = System.getenv(HOSTNAME_KEY);
+						if (envHN != null)
+							hostName = envHN;
+					} catch (Throwable th) {
+						// in case not supported by JVM/OS
+					}
+					
+					// and system property (overrides env. var.)
+					hostName = System.getProperty(HOSTNAME_KEY, hostName);
+				}
+			}
+			
+			return hostName;
+		}
+		
+	}
+    	
+	/**
+	 * Setup this handler as the only one root handler.
+	 * @param logLevel root log level to be set.
+	 */
+	public static void installPVMSLogging(Level logLevel, InetAddress sendAddress, int port) throws IOException
+	{
+		PVMSLogingHandler handler = new PVMSLogingHandler(sendAddress, port);
+		
+		LogManager.getLogManager().reset();
+		Logger rootLogger = Logger.getLogger("");
+		rootLogger.setLevel(logLevel);
+		rootLogger.addHandler(handler);
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) throws Throwable
+	{
+		final InetAddress address = InetAddress.getByName("224.0.0.1");
+		final int port = 5678;
+
+		installPVMSLogging(Level.INFO, address, port);
+		
+		
+		Logger logger = Logger.getLogger("myLogger");
+		
+		logger.info("Hello world!");
+		logger.finest("finest");
+
+		Map<String, String> props = new HashMap<String, String>();
+		props.put("entity", "myTwiss");
+		props.put("uuid", "31");
+		logger.log(Level.SEVERE, "ups, I did it again", new Object[] { props } );
+		
+		logger.log(Level.WARNING, "wow, error", new RuntimeException("rte 2"));
+		
+	}
+
+}
---
+++ pvAccessJava/src/org/epics/pvaccess/client/pvms/PVMSLoggerSubscriber.java
@@ -0,0 +1,124 @@
+package org.epics.pvaccess.client.pvms;
+
+import java.net.InetAddress;
+import java.net.SocketException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.logging.Level;
+
+import org.epics.pvdata.pv.PVField;
+import org.epics.pvdata.pv.PVInt;
+import org.epics.pvdata.pv.PVLong;
+import org.epics.pvdata.pv.PVString;
+import org.epics.pvdata.pv.PVStructure;
+
+public class PVMSLoggerSubscriber {
+	
+	/**
+	 * Line separator string. 
+	 */
+	private static String lineSeparator = System.getProperty("line.separator");
+
+	/**
+	 * ISO 8601 date formatter.
+	 */
+	private static SimpleDateFormat timeFormatter = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS");
+	
+	/**
+	 * Date object (used not to recreate it every time).
+	 */
+	private static Date date = new Date();
+	
+	public static String formatNTLogRecord(PVStructure log)
+	{
+		StringBuffer sb = new StringBuffer(128);
+	
+		PVStructure time = log.getSubField(PVStructure.class, "time");
+		PVLong secsPastEpoch = time.getSubField(PVLong.class, "secsPastEpoch");
+		PVInt nanoseconds = time.getSubField(PVInt.class, "nanoseconds");
+		long millis = secsPastEpoch.get() * 1000 + nanoseconds.get() / 1000000;
+			
+		synchronized (date)
+		{
+			date.setTime(millis);
+			sb.append(timeFormatter.format(date));
+		}
+
+		PVString loggerName = log.getSubField(PVString.class, "loggerName");
+
+		// no need to send the following two fields every time,
+		// they are the same for all the logs with same session uuid
+		PVString host = log.getSubField(PVString.class, "host");
+		PVString process = log.getSubField(PVString.class, "process");
+
+		sb.append(' ');
+		sb.append('[');
+		sb.append(process.get());
+		sb.append('@');
+		sb.append(host.get());
+		sb.append('/');
+		sb.append(loggerName.get());
+		sb.append(']');
+
+		// TODO for now java level, slow parse
+		PVInt level = log.getSubField(PVInt.class, "level");
+		Level javaLevel = Level.parse(String.valueOf(level.get()));
+		sb.append(' ');
+		sb.append('[');
+		sb.append(javaLevel);
+		sb.append(']');
+
+		PVString message = log.getSubField(PVString.class, "message");
+
+		sb.append(' ');
+		sb.append(message.get());
+		sb.append(' ');
+
+		
+		PVString stackTrace = log.getSubField(PVString.class, "stackTrace");
+		String stack = stackTrace.get();
+
+		// exceptions
+		if (stack != null && stack.length() > 0)
+		{
+			sb.append(lineSeparator);
+			sb.append(stack);
+		}
+
+		return new String(sb);
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) throws Throwable
+	{
+		final InetAddress address = InetAddress.getByName("224.0.0.1");
+		final int port = 5678;
+
+		final PVMSSubscriber subscriber = new PVMSSubscriber(address, port);
+		
+		new Thread(new Runnable() {
+			
+			@Override
+			public void run() {
+				try
+				{
+					PVField data = null;
+					while (true)
+					{
+						data = subscriber.receive(data);
+
+						System.out.println(formatNTLogRecord((PVStructure)data));
+					}
+				} catch (SocketException se) {
+					// noop for socket closed
+				} catch (Throwable th) {
+					th.printStackTrace();
+				}
+			}
+		}, "receiver-thread").start();
+		
+	}
+
+}
