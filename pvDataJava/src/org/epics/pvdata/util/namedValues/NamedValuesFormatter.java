bc512d074fbde81ff243a60be51122ba159ef8ee
Matej Sekoranja <unknown>
2014-10-13 09:23:11 +0200

    NamedValuesFormatter javadoc fixed

--- pvDataJava/src/org/epics/pvdata/util/namedValues/NamedValuesFormatter.java
+++ pvDataJava/src/org/epics/pvdata/util/namedValues/NamedValuesFormatter.java
@@ -52,8 +52,8 @@ public abstract class NamedValuesFormatter
      * 
      * @param style If style is given and is valued NamedValuesFormatter.STYLE_ROWS (1) then 
      * a row formatter is created, otherwise a column formatter is created.
-     * @see STYLE_ROWS
-     * @see STYLE_COLUMNS
+     * @see #STYLE_ROWS
+     * @see #STYLE_COLUMNS
      * @return An instance of a NamedValuesFormatter of the given style.
      */
     public static NamedValuesFormatter create( int style )
@@ -130,7 +130,7 @@ public abstract class NamedValuesFormatter
      * will include headings. If so, the names in the names/values 
      * system will be used as the heading labels.
      * 
-     * @see setWhetherDisplaylabels
+     * @see #setWhetherDisplayLabels
      * 
      * @return true if the formatter will print the labels, and false otherwise. See 
      * setWhetherDisplayLabels to change the value of the print labels setting.
@@ -202,7 +202,7 @@ public abstract class NamedValuesFormatter
      * Thinking of the namedValues system as a matrix of strings, return a given cell's string data
      * @param row Specifies the index of the vector of values that were added to the NamedValues system,
      * starting from 0 being the first name and value vector added.
-     * @param col Specifies the index of a column, where 0 indicates the name, and >0 indicates one of
+     * @param col Specifies the index of a column, where 0 indicates the name, and &gt;0 indicates one of
      * the values associated with that name.
      * @return The cell's string data.
      */
@@ -214,7 +214,7 @@ public abstract class NamedValuesFormatter
      * or row formatting.
      * @param row Specifies the index of the vector of values that were added to the NamedValues system,
      * starting from 0 being the first name and value vector added.
-     * @param col Specifies the index of a column, where 0 indicates the name, and >0 indicates one of
+     * @param col Specifies the index of a column, where 0 indicates the name, and &gt;0 indicates one of
      * the values associated with that name.
      * @return The cell's string data, formatted appropriately as being printed for the system as a whole.
      */
