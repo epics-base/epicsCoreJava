eb66e48d49d9a23e58974a192672f34cf71b8ebd
Dave Hickin <david.hickin@diamond.ac.uk>
2015-08-20 21:42:37 +0100

    Add NTHistogram

---
+++ normativeTypesJava/src/org/epics/nt/NTHistogram.java
@@ -0,0 +1,238 @@
+/**
+ * Copyright - See the COPYRIGHT that is included with this distribution.
+ * EPICS pvData is distributed subject to a Software License Agreement found
+ * in file LICENSE that is included with this distribution.
+ */
+package org.epics.nt;
+
+import org.epics.pvdata.pv.Structure;
+import org.epics.pvdata.pv.PVField;
+import org.epics.pvdata.pv.PVDoubleArray;
+import org.epics.pvdata.pv.PVScalarArray;
+import org.epics.pvdata.pv.PVStructure;
+import org.epics.pvdata.pv.PVString;
+import org.epics.pvdata.property.PVTimeStamp;
+import org.epics.pvdata.property.PVAlarm;
+
+/**
+ * Wrapper class for NTHistogram
+ *
+ * @author dgh
+ */
+public class NTHistogram
+    implements HasTimeStamp, HasAlarm
+{
+    public static final String URI = "epics:nt/NTHistogram:1.0";
+
+    /**
+     * Creates an NTHistogram wrapping the specified PVStructure if the latter is compatible.
+     *
+     * Checks the supplied structure is compatible with NTHistogram
+     * and if so returns a NTHistogram which wraps it.
+     * This method will return null if the structure is is not compatible
+     * or is null.
+     * @param pvStructure The PVStructure to be wrapped.
+     * @return NTHistogram instance on success, null otherwise.
+     */
+    public static NTHistogram wrap(PVStructure pvStructure)
+    {
+        if (!isCompatible(pvStructure))
+            return null;
+        return wrapUnsafe(pvStructure);
+    }
+
+    /**
+     * Creates an NTHistogram wrapping the specified PVStructure, regardless of the latter's compatibility.
+     *
+     * No checks are made as to whether the specified PVStructure
+     * is compatible with NTHistogram or is non-null.
+     * @param pvStructure The PVStructure to be wrapped.
+     * @return NTHistogram instance.
+     */
+    public static NTHistogram wrapUnsafe(PVStructure pvStructure)
+    {
+        return new NTHistogram(pvStructure);
+    }
+
+    /**
+     * Checks if the specified structure reports to be a compatible NTHistogram.
+     *
+     * Checks whether the specified structure reports compatibility with this
+     * version of NTHistogram through type ID, including checking version numbers.
+     * The return value does not depend on whether the structure is actually
+     * compatible in terms of its introspection type
+     * @param structure The pvStructure to test.
+     * @return (false,true) if (is not, is) a compatible NTHistogram.
+     */
+    public static boolean is_a(Structure structure)
+    {
+        return NTUtils.is_a(structure.getID(), URI);
+    }
+
+    /**
+     * Checks if the specified structure is compatible with NTHistogram.
+     *
+     * Checks whether the specified structure is compatible with this version
+     * of NTHistogram through introspection interface.
+     * @param pvStructure The PVStructure to test.
+     * @return (false,true) if (is not, is) a compatible NTHistogram.
+     */
+    public static boolean isCompatible(PVStructure pvStructure)
+    {
+        if (pvStructure == null) return false;
+
+        PVField pvField = pvStructure.getSubField("ranges");
+        if (pvField != null && pvStructure.getSubField(PVDoubleArray.class, "ranges") == null)
+            return false;
+
+        PVScalarArray pvValue = pvStructure.getSubField(PVDoubleArray.class, "value");
+        if (pvValue == null) return false;
+        switch(pvValue.getScalarArray().getElementType())
+        {
+        case pvShort:
+        case pvInt:
+        case pvLong:
+            break;
+        default:
+            return false;
+        }
+
+        pvField = pvStructure.getSubField("descriptor");
+        if (pvField != null && pvStructure.getSubField(PVString.class, "descriptor") == null)
+            return false;
+
+        NTField ntField = NTField.get();
+
+        pvField = pvStructure.getSubField("alarm");
+        if (pvField != null  && !ntField.isAlarm(pvField.getField()))
+            return false;
+
+        pvField = pvStructure.getSubField("timeStamp");
+        if (pvField != null && !ntField.isTimeStamp(pvField.getField()))
+            return false;
+
+        return true;
+    }
+
+    /**
+     * Create a NTHistogram builder instance.
+     * @return builder instance.
+     */
+    public static NTHistogramBuilder createBuilder()
+    {
+        return new NTHistogramBuilder();
+    }
+
+    /**
+     * Get the pvStructure.
+     * @return PVStructure.
+     */
+    public PVStructure getPVStructure()
+    {
+        return pvNTHistogram;
+    }
+
+    /**
+     * Get the ranges field.
+     * @return The PVDoubleArray for the values.
+     */
+    public PVDoubleArray getRanges()
+    {
+        return pvNTHistogram.getSubField(PVDoubleArray.class, "ranges");
+    }
+
+    /**
+     * Get the value field.
+     * @return The PVScalarArray for the values.
+     */
+    public PVScalarArray getValue()
+    {
+        return pvValue;
+    }
+
+
+    /* Get the value field of a specified type.
+     * @param c expected class of a requested field (must be PVShortArray, PVIntArray or PVLongArray).
+     * @return The PVScalarArray or null if the subfield does not exist, or the field is not of <code>c</code> type.
+     */
+    public <T extends PVScalarArray> T getValue(Class<T> c)
+    {
+        if (c.isInstance(pvValue))
+            return c.cast(pvValue);
+        else
+            return null;
+    }
+
+
+    /**
+     * Get the descriptor field.
+     * @return The pvString or null if no function field.
+     */
+    public PVString getDescriptor()
+    {
+        return pvNTHistogram.getSubField(PVString.class, "descriptor");
+    }
+
+    /* (non-Javadoc)
+	 * @see org.epics.pvdata.nt.HasAlarm#getAlarm()
+	 */
+    public PVStructure getAlarm()
+    {
+       return pvNTHistogram.getSubField(PVStructure.class, "alarm");
+    }
+
+    /* (non-Javadoc)
+	 * @see org.epics.pvdata.nt.HasTimeStamp#getTimeStamp()
+	 */
+    public PVStructure getTimeStamp()
+    {
+        return pvNTHistogram.getSubField(PVStructure.class, "timeStamp");
+    }
+
+    /* (non-Javadoc)
+	 * @see org.epics.pvdata.nt.HasAlarm#attachAlarm(org.epics.pvdata.property.PVAlarm)
+	 */
+    public boolean attachAlarm(PVAlarm pvAlarm)
+    {
+        PVStructure al = getAlarm();
+        if (al != null)
+            return pvAlarm.attach(al);
+        else
+            return false;
+    }
+
+    /* (non-Javadoc)
+	 * @see org.epics.pvdata.nt.HasTimeStamp#attachTimeStamp(org.epics.pvdata.property.PVTimeStamp)
+	 */
+    public boolean attachTimeStamp(PVTimeStamp pvTimeStamp)
+    {
+        PVStructure ts = getTimeStamp();
+        if (ts != null)
+            return pvTimeStamp.attach(ts);
+        else
+            return false;
+    }
+
+    /* (non-Javadoc)
+     * @see java.lang.Object#toString()
+     */ 
+    public String toString()
+    {
+        return getPVStructure().toString();
+    }
+
+    /**
+     * Constructor
+     * @param pvStructure The PVStructure to be wrapped.
+     */
+    NTHistogram(PVStructure pvStructure)
+    {
+        pvNTHistogram = pvStructure;
+        pvValue = pvNTHistogram.getSubField(PVScalarArray.class, "value");
+    }
+
+    private PVStructure pvNTHistogram;
+    private PVScalarArray pvValue;
+}
+
+
---
+++ normativeTypesJava/src/org/epics/nt/NTHistogramBuilder.java
@@ -0,0 +1,165 @@
+/**
+ * Copyright - See the COPYRIGHT that is included with this distribution.
+ * EPICS pvData is distributed subject to a Software License Agreement found
+ * in file LICENSE that is included with this distribution.
+ */
+package org.epics.nt;
+
+import org.epics.pvdata.pv.Field;
+import org.epics.pvdata.pv.FieldBuilder;
+import org.epics.pvdata.pv.FieldCreate;
+import org.epics.pvdata.pv.ScalarType;
+import org.epics.pvdata.pv.Structure;
+import org.epics.pvdata.pv.PVStructure;
+import org.epics.pvdata.factory.FieldFactory;
+import org.epics.pvdata.factory.PVDataFactory;
+
+import java.util.ArrayList;
+
+/**
+ * Interface for in-line creating of NTHistogram.
+ *
+ * One instance can be used to create multiple instances.
+ * An instance of this object must not be used concurrently (an object has a state).
+ * @author dgh
+ */
+public class NTHistogramBuilder
+{
+    /**
+     * Set a value type of a NTHistogram.
+     * @param scalarType the value type.
+     * @return this instance of <b>NTHistogramBuilder</b>.
+     */
+    public NTHistogramBuilder value(ScalarType scalarType)
+    {
+        valueType = scalarType;
+        valueTypeSet = true;
+        return this;
+    }
+
+    /**
+     * Add descriptor field to the NTHistogram.
+     * @return this instance of <b>NTHistogramBuilder</b>.
+     */
+    public NTHistogramBuilder addDescriptor()
+    {
+        descriptor = true;
+        return this;
+    }
+
+    /**
+     * Add alarm structure to the NTHistogram.
+     * @return this instance of <b>NTHistogramBuilder</b>.
+     */
+    public NTHistogramBuilder addAlarm()
+    {
+        alarm = true;
+        return this;
+    }
+
+    /**
+     * Add timeStamp structure to the NTHistogram.
+     * @return this instance of <b>NTHistogramBuilder</b>.
+     */
+    public NTHistogramBuilder addTimeStamp()
+    {
+        timeStamp = true;
+        return this;
+    }
+
+    /**
+     * Create a <b>Structure</b> that represents NTHistogram.
+     * This resets this instance state and allows new instance to be created.
+     * @return a new instance of a <b>Structure</b>.
+     */
+    public Structure createStructure()
+    {
+        if (!valueTypeSet)
+            throw new RuntimeException("value type not set");
+
+        FieldBuilder builder =
+            FieldFactory.getFieldCreate().createFieldBuilder().
+               setId(NTHistogram.URI).
+               addArray("ranges", ScalarType.pvDouble).
+               addArray("value", valueType);
+
+        NTField ntField = NTField.get();
+
+        if (descriptor)
+            builder.add("descriptor", ScalarType.pvString);
+
+        if (alarm)
+            builder.add("alarm", ntField.createAlarm());
+
+        if (timeStamp)
+            builder.add("timeStamp", ntField.createTimeStamp());
+
+        int extraCount = extraFieldNames.size();
+        for (int i = 0; i< extraCount; i++)
+            builder.add(extraFieldNames.get(i), extraFields.get(i));
+
+        Structure s = builder.createStructure();
+
+        reset();
+        return s;
+    }
+
+    /**
+     * Create a <b>PVStructure</b> that represents NTHistogram.
+     * This resets this instance state and allows new instance to be created.
+     * @return a new instance of a <b>PVStructure</b>.
+     */
+    public PVStructure createPVStructure()
+    {
+        return PVDataFactory.getPVDataCreate().createPVStructure(createStructure());
+    }
+
+    /**
+     * Create a <b>NTHistogram</b> instance.
+     * This resets this instance state and allows new instance to be created.
+     * @return a new instance of a <b>NTHistogram</b>.
+     */
+    public NTHistogram create()
+    {
+        return new NTHistogram(createPVStructure());
+    }
+
+    /**
+     * Add extra <b>Field</b> to the type.
+     * @param name name of the field.
+     * @param field a field to add.
+     * @return this instance of <b>NTHistogramBuilder</b>.
+     */
+    public NTHistogramBuilder add(String name, Field field) 
+    {
+        extraFields.add(field);
+        extraFieldNames.add(name);
+        return this;
+    }
+
+    NTHistogramBuilder()
+    {
+        reset();
+    }
+
+    private void reset()
+    {
+        valueTypeSet = false;
+        descriptor = false;
+        alarm = false;
+        timeStamp = false;
+        extraFieldNames.clear();
+        extraFields.clear();
+    }
+    private boolean valueTypeSet;
+    private ScalarType valueType;
+
+    private boolean descriptor;
+    private boolean alarm;
+    private boolean timeStamp;
+
+    // NOTE: this preserves order, however it does not handle duplicates
+    private ArrayList<String> extraFieldNames = new ArrayList<String>();
+    private ArrayList<Field> extraFields = new ArrayList<Field>();
+}
+
