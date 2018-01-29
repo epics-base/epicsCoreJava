854ecf4f16d85ea998e93a552dad9c46e3f5eacf
Gabriele Carcassi <gabriele.carcassi@gmail.com>
2018-01-29 14:07:08 -0500

    epics-gpclient: adding reliable way to test collector events

---
+++ gpclient/gpclient/src/test/java/org/epics/gpclient/expression/ProbeCollector.java
@@ -0,0 +1,95 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.epics.gpclient.expression;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicReference;
+import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.epics.gpclient.expression.LatestValueCollector;
+import org.epics.gpclient.expression.ReadCollector;
+import org.epics.gpclient.expression.SourceRateReadEvent;
+
+/**
+ *
+ * @author carcassi
+ */
+public class ProbeCollector<T> {
+    
+    private final ReadCollector<T, T> collector;
+    private final Object lock = new Object();
+    private final List<SourceRateReadEvent> events = new ArrayList<>();
+    private Runnable test;
+
+    public ProbeCollector(Class<T> type) {
+        this.collector = new LatestValueCollector<>(type);
+        this.collector.setUpdateListener(new Consumer<SourceRateReadEvent>() {
+            @Override
+            public void accept(SourceRateReadEvent event) {
+                synchronized(lock) {
+                    events.add(event);
+                    if (test != null) {
+                        test.run();
+                    }
+                }
+            }
+        });
+    }
+
+    public ReadCollector<T, T> getCollector() {
+        return collector;
+    }
+
+    public List<SourceRateReadEvent> getEvents() {
+        return events;
+    }
+
+    public void wait(int ms, Function<List<SourceRateReadEvent>, Boolean> condition) {
+        CountDownLatch latch = new CountDownLatch(1);
+        Runnable newTest = new Runnable() {
+            @Override
+            public void run() {
+                if (condition.apply(events)) {
+                    latch.countDown();
+                }
+            }
+        };
+        synchronized(lock) {
+            test = newTest;
+            test.run();
+        }
+        
+        boolean success = false;
+        
+        try {
+            success = latch.await(ms, TimeUnit.MILLISECONDS);
+        } catch (InterruptedException ex) {
+        }
+        
+        if (!success) {
+            throw new AssertionError("Waited but " + condition + " but was not satisfied");
+        }
+    }
+    
+    public static Function<List<SourceRateReadEvent>, Boolean> forAnEvent() {
+        return (List<SourceRateReadEvent> list) -> !list.isEmpty();
+    }
+    
+    public static Function<List<SourceRateReadEvent>, Boolean> forEventCount(final int count) {
+        return (List<SourceRateReadEvent> list) -> list.size() >= count;
+    }
+    
+    public static ProbeCollector<?> create() {
+        return new ProbeCollector<>(Object.class);
+    }
+    
+}
