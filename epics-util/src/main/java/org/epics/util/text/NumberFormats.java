d0b3d81a9220799f486dc1138800dc8997940193
Gabriele Carcassi <gabriele.carcassi@gmail.com>
2018-04-05 11:21:30 -0400

    util: adding printf based formats

--- epics-util/src/main/java/org/epics/util/text/NumberFormats.java
+++ epics-util/src/main/java/org/epics/util/text/NumberFormats.java
@@ -19,11 +19,8 @@ import java.util.concurrent.ConcurrentHashMap;
  * @author carcassi
  */
 public final class NumberFormats {
-
-    private static final Map<Integer, DecimalFormat> precisionFormat =
-            new ConcurrentHashMap<Integer, DecimalFormat>();
-    private static volatile Locale currentLocale;
-    private static volatile DecimalFormatSymbols symbols;
+    private static final Locale currentLocale;
+    private static final DecimalFormatSymbols symbols;
     
     static {
         Locale newLocale = Locale.getDefault();
@@ -38,6 +35,30 @@ public final class NumberFormats {
         // Prevent instances
     }
 
+    private static final Map<Integer, DecimalFormat> precisionFormats =
+            new ConcurrentHashMap<Integer, DecimalFormat>();
+
+    /**
+     * Creates a new number format that formats a number with the given
+     * number of precision digits.
+     *
+     * @param precision number of digits past the decimal point
+     * @return a number format
+     */
+    private static DecimalFormat createPrecisionFormat(int precision) {
+        if (precision < 0)
+            throw new IllegalArgumentException("Precision must be non-negative");
+
+        if (precision == 0)
+            return new DecimalFormat("0", symbols);
+
+        StringBuilder sb = new StringBuilder("0.");
+        for (int i = 0; i < precision; i++) {
+            sb.append("0");
+        }
+        return new DecimalFormat(sb.toString(), symbols);
+    }
+
     /**
      * Returns a number format that formats a number with the given
      * number of precision digits. Parsing is not currently supported.
@@ -45,12 +66,12 @@ public final class NumberFormats {
      * @param precision number of digits past the decimal point
      * @return a number format
      */
-    public static NumberFormat format(int precision) {
-        NumberFormat format = precisionFormat.get(precision);
+    public static NumberFormat precisionFormat(int precision) {
+        NumberFormat format = precisionFormats.get(precision);
         if (format == null) {
-            precisionFormat.put(precision, createFormatter(precision));
+            precisionFormats.put(precision, createPrecisionFormat(precision));
         }
-        return precisionFormat.get(precision);
+        return precisionFormats.get(precision);
     }
     
     private static final NumberFormat toStringFormat = new NumberFormat() {
@@ -83,25 +104,66 @@ public final class NumberFormats {
         return toStringFormat;
     }
 
-    /**
-     * Creates a new number format that formats a number with the given
-     * number of precision digits.
-     *
-     * @param precision number of digits past the decimal point
-     * @return a number format
-     */
-    private static DecimalFormat createFormatter(int precision) {
-        if (precision < 0)
-            throw new IllegalArgumentException("Precision must be non-negative");
+    @SuppressWarnings("serial")
+    static class PrintfFormat extends java.text.NumberFormat {
 
-        if (precision == 0)
-            return new DecimalFormat("0", symbols);
+        private final String format;
 
-        StringBuilder sb = new StringBuilder("0.");
-        for (int i = 0; i < precision; i++) {
-            sb.append("0");
+        public PrintfFormat(String printfFormat) {
+            // probe precisionFormat
+            boolean allOK = true;
+            try {
+                String.format(currentLocale, printfFormat, 0.0);
+            } catch (Throwable th) {
+                allOK = false;
+            }
+            // accept it if all is OK
+            this.format = allOK ? printfFormat : null;
         }
-        return new DecimalFormat(sb.toString(), symbols);
-    }
 
+        private String internalFormat(double number) {
+            if (format != null) {
+                return String.format(currentLocale, format, number);
+            } else {
+                return String.valueOf(number);
+            }
+        }
+
+        @Override
+        public StringBuffer format(double number, StringBuffer toAppendTo, FieldPosition pos) {
+            toAppendTo.append(internalFormat(number));
+            return toAppendTo;
+        }
+
+        @Override
+        public StringBuffer format(long number, StringBuffer toAppendTo, FieldPosition pos) {
+            toAppendTo.append(internalFormat(number));
+            return toAppendTo;
+        }
+
+        @Override
+        public Number parse(String source, ParsePosition parsePosition) {
+            throw new UnsupportedOperationException("Not supported yet.");
+        }
+    };
+
+    private static final Map<String, NumberFormat> printfFormats =
+            new ConcurrentHashMap<String, NumberFormat>();
+
+    public static NumberFormat printfFormat(String format) {
+        if (format == null
+                || format.trim().isEmpty()
+                || format.equals("%s")) {
+            return NumberFormats.toStringFormat();
+        } else {
+            NumberFormat printfFormat = printfFormats.get(format);
+            if (printfFormat != null) {
+                return printfFormat;
+            } else {
+                printfFormat = new PrintfFormat(format);
+                printfFormats.put(format, printfFormat);
+                return printfFormat;
+            }
+        }
+    }
 }
--- epics-util/src/test/java/org/epics/util/text/NumberFormatsTest.java
+++ epics-util/src/test/java/org/epics/util/text/NumberFormatsTest.java
@@ -21,27 +21,27 @@ public class NumberFormatsTest {
 
     @Test
     public void format1() {
-        NumberFormat format = NumberFormats.format(2);
+        NumberFormat format = NumberFormats.precisionFormat(2);
         assertThat(format.format(2.0), equalTo("2.00"));
         assertThat(format.format(Double.NaN), equalTo("NaN"));
         assertThat(format.format(Double.POSITIVE_INFINITY), equalTo("Infinity"));
         assertThat(format.format(Double.NEGATIVE_INFINITY), equalTo("-Infinity"));
-        assertThat(NumberFormats.format(2), sameInstance(format));
+        assertThat(NumberFormats.precisionFormat(2), sameInstance(format));
     }
     
     @Test
     public void format2() {
-        NumberFormat f = NumberFormats.format(3);
+        NumberFormat f = NumberFormats.precisionFormat(3);
         assertThat(f.format(1234.4567), equalTo("1234.457"));
         assertThat(f.format(123), equalTo("123.000"));
         assertThat(f.format(123.4), equalTo("123.400"));
 
-        f = NumberFormats.format(0);
+        f = NumberFormats.precisionFormat(0);
         assertThat(f.format(1234.4567), equalTo("1234"));
         assertThat(f.format(123), equalTo("123"));
         assertThat(f.format(123.4), equalTo("123"));
 
-        f = NumberFormats.format(4);
+        f = NumberFormats.precisionFormat(4);
         assertThat(f.format(1234.4567), equalTo("1234.4567"));
         assertThat(f.format(123), equalTo("123.0000"));
         assertThat(f.format(123.4), equalTo("123.4000"));
@@ -55,5 +55,15 @@ public class NumberFormatsTest {
         assertThat(format.format(Double.POSITIVE_INFINITY), equalTo("Infinity"));
         assertThat(format.format(Double.NEGATIVE_INFINITY), equalTo("-Infinity"));
         assertThat(NumberFormats.toStringFormat(), sameInstance(format));
+    }
+
+    @Test
+    public void printfFormat1() {
+        NumberFormat format = NumberFormats.printfFormat("Value: %10.2f mm");
+        assertThat(format.format(2.0), equalTo("Value:       2.00 mm"));
+        assertThat(format.format(Double.NaN), equalTo("Value:        NaN mm"));
+        assertThat(format.format(Double.POSITIVE_INFINITY), equalTo("Value:   Infinity mm"));
+        assertThat(format.format(Double.NEGATIVE_INFINITY), equalTo("Value:  -Infinity mm"));
+        assertThat(NumberFormats.printfFormat("Value: %10.2f mm"), sameInstance(format));
     }
 }
