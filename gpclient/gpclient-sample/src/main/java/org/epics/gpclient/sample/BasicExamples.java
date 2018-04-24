3482d312647b35339c482973c1539c6153c53eee
Gabriele Carcassi <gabriele.carcassi@gmail.com>
2018-04-24 10:17:05 -0400

    gpclient: improving the examples

--- gpclient/gpclient-sample/src/main/java/org/epics/gpclient/sample/BasicExamples.java
+++ gpclient/gpclient-sample/src/main/java/org/epics/gpclient/sample/BasicExamples.java
@@ -4,6 +4,10 @@
  */
 package org.epics.gpclient.sample;
 
+import java.time.Duration;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import org.epics.gpclient.GPClient;
 import org.epics.gpclient.PVEvent;
 import org.epics.gpclient.PVReader;
@@ -17,38 +21,93 @@ import org.epics.gpclient.PV;
  */
 public class BasicExamples {
 
-    public static void b1_readLatestValue() throws Exception {
-        PVReader<VType> pv = GPClient.read("sim://delayedConnectionChannel(3, \"init\")")
+    public static void simpleRead() {
+        PVReader<VType> pv = GPClient.read("sim://noise")
+                .addListener((PVEvent event, PVReader<VType> p) -> {
+                    System.out.println(event + " " + p.isConnected() + " " + p.getValue());
+                })
+                .start();
+
+        pause(2000);
+
+        pv.close();
+    }
+
+    public static void simpleReadAndWrite() {
+        PV<VType, Object> pv = GPClient.readAndWrite(channel("loc://a(\"init\")"))
+                .addListener((PVEvent event, PV<VType, Object> p) -> {
+                    System.out.println(event + " " + p.isConnected() + " " + p.isWriteConnected() + " " + p.getValue());
+                })
+                .start();
+
+        pause(1000);
+
+        pv.write("New Value");
+
+        pause(1000);
+
+        pv.close();
+    }
+
+    public static void connectionTimeout() {
+        PVReader<VType> pv = GPClient.read("sim://delayedConnectionChannel(2, \"init\")")
                 .addListener((PVEvent event, PVReader<VType> pvReader) -> {
+                    System.out.println(event + " " + pvReader.isConnected() + " " + pvReader.getValue());
+                })
+                .connectionTimeout(Duration.ofSeconds(1))
+                .start();
+
+        pause(2500);
+
+        pv.close();
+    }
+
+    public static void readLatestValue() {
+        PVReader<VType> pv = GPClient.read("sim://noise(-5,5,0.01)")
+                .addListener((PVEvent event, PVReader<VType> p) -> {
+                    System.out.println(event + " " + p.isConnected() + " " + p.getValue());
+                })
+                .start();
+
+        pause(2000);
+
+        pv.close();
+    }
+
+    public static void readAllValues() {
+        PVReader<List<VType>> pv = GPClient.read(channel("sim://noise(-5,5,0.01)", queueAllValues(VType.class)))
+                .addListener((PVEvent event, PVReader<List<VType>> pvReader) -> {
                     System.out.println(event + " " + pvReader.isConnected() + " " + pvReader.getValue());
                 })
                 .start();
 
-        Thread.sleep(10000);
+        pause(2000);
 
-        GPClient.defaultInstance().getDefaultDataSource().close();
+        pv.close();
     }
 
-    public static void b1_readAndWriteLoc() throws Exception {
-        PV<VType, Object> pv = GPClient.readAndWrite(channel("loc://a(\"init\")"))
-                .addListener((PVEvent event, PV<VType, Object> pvReader) -> {
-                    System.out.println(event + " " + pvReader.isConnected() + " " + pvReader.isWriteConnected() + " " + pvReader.getValue());
-                    if (event.isType(PVEvent.Type.EXCEPTION)) {
-                        event.getException().printStackTrace();
-                    }
-                })
-                .start();
+    public static void run(String title, Runnable test) {
+        System.out.println(title);
+        System.out.println(title.replaceAll(".", "-"));
+        test.run();
+        System.out.println("");
+    }
 
-        Thread.sleep(1000);
+    public static void pause(int millis) {
+        try {
+            Thread.sleep(millis);
+        } catch (InterruptedException ex) {
+            Logger.getLogger(BasicExamples.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    public static void main(String[] args) {
+        run("Simple read", BasicExamples::simpleRead);
+        run("Simple read and write", BasicExamples::simpleReadAndWrite);
+        run("Connection timeout", BasicExamples::connectionTimeout);
+        run("Read latest value", BasicExamples::readLatestValue);
+        run("Read all values", BasicExamples::readAllValues);
         
-        pv.write("New Value");
-
-        Thread.sleep(1000);
-
         GPClient.defaultInstance().getDefaultDataSource().close();
-    }
-
-    public static void main(String[] args) throws Exception {
-        b1_readLatestValue();
     }
 }
