967802429c0edcd077c03505b0b53bc43367c2e7
Gabriele Carcassi <gabriele.carcassi@gmail.com>
2018-05-02 10:44:43 -0400

    gpclient: adding black box tests

--- gpclient/gpclient-sample/src/test/java/WriteTest.java
+++ gpclient/gpclient-sample/src/test/java/WriteTest.java
@@ -3,24 +3,20 @@
  * Copyright information and license terms for this software can be
  * found in the file LICENSE.TXT included with the distribution.
  */
-
 import java.time.Duration;
-import java.util.Arrays;
-import java.util.concurrent.Callable;
-import java.util.concurrent.ScheduledExecutorService;
-import java.util.logging.Handler;
-import java.util.logging.Level;
-import java.util.logging.Logger;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
 import org.epics.gpclient.GPClientConfiguration;
 import org.epics.gpclient.GPClientInstance;
 import org.epics.gpclient.PV;
 import org.epics.gpclient.PVEvent;
 import org.epics.gpclient.PVListener;
+import org.epics.gpclient.PVWriter;
 import org.epics.gpclient.datasource.DataSourceProvider;
 import org.junit.Test;
 import static org.junit.Assert.*;
 import static org.hamcrest.Matchers.*;
-import org.epics.util.concurrent.Executors;
+import org.epics.vtype.VString;
 import org.epics.vtype.VType;
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
@@ -56,5 +52,70 @@ public class WriteTest {
                 .start();
         assertThat(pv.isWriteConnected(), equalTo(false));
         pv.write("Value");
+    }
+
+    @Test(expected = RuntimeException.class)
+    public void writeDisconnectedChannel() {
+        PV<VType, Object> pv = gpClient.readAndWrite("loc://a")
+                .addListener((PVListener<VType, Object>) (PVEvent event, PV<VType, Object> pv1) -> {
+                    // Do nothing
+                })
+                .start();
+        assertThat(pv.isWriteConnected(), equalTo(false));
+        pv.write("Value");
+    }
+
+    @Test(expected = RuntimeException.class)
+    public void writeDisconnectedChannelAsynch() {
+        PV<VType, Object> pv = gpClient.readAndWrite("loc://a")
+                .addListener((PVEvent event, PV<VType, Object> pv1) -> {
+                    // Do nothing
+                })
+                .start();
+        assertThat(pv.isWriteConnected(), equalTo(false));
+        pv.write("Value", (PVEvent event, PVWriter<Object> pv1) -> {
+            // Do nothing
+        });
+    }
+
+    @Test(expected = RuntimeException.class)
+    public void writeDisconnectedChannelSynch() {
+        PV<VType, Object> pv = gpClient.readAndWrite("loc://a")
+                .addListener((PVListener<VType, Object>) (PVEvent event, PV<VType, Object> pv1) -> {
+                    // Do nothing
+                })
+                .start();
+        assertThat(pv.isWriteConnected(), equalTo(false));
+        pv.writeAndWait("Value");
+    }
+
+    @Test
+    public void writeChannelSynch() throws InterruptedException {
+        CountDownLatch connectionLatch = new CountDownLatch(1);
+        CountDownLatch valueLatch = new CountDownLatch(1);
+        PV<VType, Object> pv = gpClient.readAndWrite("loc://a")
+                .addListener((PVListener<VType, Object>) (PVEvent event, PV<VType, Object> pv1) -> {
+                    if (event.isType(PVEvent.Type.WRITE_CONNECTION)) {
+                        connectionLatch.countDown();
+                    }
+                    if (event.isType(PVEvent.Type.VALUE)) {
+                        valueLatch.countDown();
+                    }
+                })
+                .start();
+        assertThat(pv.isWriteConnected(), equalTo(false));
+        awaitTimeout(connectionLatch, Duration.ofSeconds(1));
+        assertThat(pv.getValue(), equalTo(null));
+        System.out.println("Writing");
+        pv.writeAndWait("Value");
+        awaitTimeout(valueLatch, Duration.ofSeconds(1));
+        assertThat(pv.getValue(), instanceOf(VString.class));
+        assertThat(((VString) pv.getValue()).getValue(), equalTo("Value"));
+    }
+    
+    public static void awaitTimeout(CountDownLatch latch, Duration duration) throws InterruptedException {
+        if (!latch.await(duration.toMillis(), TimeUnit.MILLISECONDS)) {
+            fail("Latch didn't count to zero");
+        }
     }
 }
