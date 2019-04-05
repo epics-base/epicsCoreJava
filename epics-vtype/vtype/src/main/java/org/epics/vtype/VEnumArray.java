8d949ca07eed7bb4c4a831ba6448d4a67c35a4d5
Kunal Shroff <kunalshroff9@gmail.com>
2019-04-05 14:27:04 -0400

    Update VEnumArray to allow indices with no label string and short[]

--- epics-vtype/vtype/src/main/java/org/epics/vtype/VEnumArray.java
+++ epics-vtype/vtype/src/main/java/org/epics/vtype/VEnumArray.java
@@ -3,11 +3,16 @@ package org.epics.vtype;
 import java.util.List;
 
 import org.epics.util.array.ListInteger;
+import org.epics.util.array.ListNumber;
+import org.epics.util.array.ListShort;
 
+/**
+ * Scalar enum array with alarm, timestamp, and display information.
+ */
 public abstract class VEnumArray extends Array implements AlarmProvider, TimeProvider {
 
     /**
-     * 
+     * Return the enum label values
      */
     @Override
     public abstract List<String> getData();
@@ -17,7 +22,7 @@ public abstract class VEnumArray extends Array implements AlarmProvider, TimePro
      *
      * @return an array of indexes
      */
-    public abstract ListInteger getIndexes();
+    public abstract ListNumber getIndexes();
 
     /** @return the enum display information, i.e. choices */
     public abstract EnumDisplay getDisplay();
@@ -31,6 +36,18 @@ public abstract class VEnumArray extends Array implements AlarmProvider, TimePro
      * @return {@link VEnumArray} instance of VEnumArray
      */
     public static VEnumArray of(ListInteger data, EnumDisplay enumDisplay, Alarm alarm, Time time) {
+        return new IVEnumArray(data, enumDisplay, alarm, time);
+    }
+
+    /**
+     * Return an instance of the VEnumArray
+     * @param data the indices
+     * @param enumDisplay the enum display labels
+     * @param alarm the alarm
+     * @param time new time
+     * @return {@link VEnumArray} instance of VEnumArray
+     */
+    public static VEnumArray of(ListShort data, EnumDisplay enumDisplay, Alarm alarm, Time time) {
         return new IVEnumArray(data, enumDisplay, alarm, time);
     }
 }
